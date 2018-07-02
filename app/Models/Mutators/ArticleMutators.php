a2907e20c3393f1866dcaed85e2b580d5615a943
Рустам <rusiq@list.ru>
2018-07-02 17:15:21 +1000

    Даты записей в формате ISO 8601 для микроразметки
    {{ $article->date_created }}
    {{ $article->date_published }}
    {{ $article->date_modified }}

--- app/Models/Mutators/ArticleMutators.php
+++ app/Models/Mutators/ArticleMutators.php
@@ -2,6 +2,10 @@
 
 namespace BBCMS\Models\Mutators;
 
+/**
+ * https://schema.org/Article
+ */
+
 trait ArticleMutators
 {
     public function setSlugAttribute($value)
@@ -14,18 +18,53 @@ trait ArticleMutators
         return action('ArticlesController@article', [$this->categories->pluck('slug')->implode('/'), $this->id, $this->slug]);
     }
 
+    public function getTeaserAttribute()
+    {
+        return $this->attributes['teaser'] ?? teaser($this->content, setting('articles.teaser_length', 150));
+    }
+
+    /**
+     * Get `created` in humans date format.
+     * @return mixed
+     */
     public function getCreatedAttribute()
     {
         return is_null($this->attributes['created_at']) ? null : $this->created_at->diffForHumans();
     }
 
+    /**
+     * Get `updated` in humans date format.
+     * @return mixed
+     */
     public function getUpdatedAttribute()
     {
         return is_null($this->attributes['updated_at']) ? null : $this->updated_at->diffForHumans();
     }
 
-    public function getTeaserAttribute()
+    /**
+     * Get `date_created` in ISO 8601 date format.
+     * @return mixed
+     */
+    public function getDateCreatedAttribute()
     {
-        return $this->attributes['teaser'] ?? teaser($this->content, setting('articles.teaser_length', 150));
+        return is_null($this->attributes['created_at']) ? null : $this->created_at->toIso8601String();
+    }
+
+    /**
+     * Get `date_published` in ISO 8601 date format.
+     * @return mixed
+     */
+    public function getDateCreatedAttribute()
+    {
+        return is_null($this->attributes['published_at']) ? null : $this->published_at->toIso8601String();
+    }
+
+    /**
+     * Get `date_modified` in ISO 8601 date format.
+     * @return mixed
+     */
+    public function getDateModifiedAttribute()
+    {
+        return is_null($this->attributes['updated_at']) ? null : $this->updated_at->toIso8601String();
     }
 }
