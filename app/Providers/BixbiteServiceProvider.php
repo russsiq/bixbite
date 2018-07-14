3d8bf701bea5e899d46dcdd0203fda866abaa692
Рустам <rusiq@list.ru>
2018-07-14 16:23:26 +1000

    попутно зацепил

--- app/Providers/BixbiteServiceProvider.php
+++ app/Providers/BixbiteServiceProvider.php
@@ -23,36 +23,6 @@ class BixbiteServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-
-        // if ($modules = setting('modules')) {
-        //
-        //     $path = app()->basePath('modules').DS;
-        //
-        //     foreach ($modules as $key => $module) {
-        //
-        //         if (file_exists($path.$module.DS.'Routes/routes.php')) {
-        //             $this->loadRoutesFrom(__DIR__.'/'.$module.'/Routes/routes.php');
-        //         }
-        //
-        //         //Загружаем View
-        //         //view('Test::admin')
-        //         if (is_dir(__DIR__.'/'.$module.'/Views')) {
-        //             $this->loadViewsFrom(__DIR__.'/'.$module.'/Views', $module);
-        //         }
-        //
-        //         //Подгружаем миграции
-        //         if (is_dir(__DIR__.'/'.$module.'/Migration')) {
-        //             $this->loadMigrationsFrom(__DIR__.'/'.$module.'/Migration');
-        //         }
-        //
-        //         //Подгружаем переводы
-        //         //trans('Test::messages.welcome')
-        //         if (is_dir(__DIR__.'/'.$module.'/Lang')) {
-        //             $this->loadTranslationsFrom(__DIR__.'/'.$module.'/Lang', $module);
-        //         }
-        //     }
-        // }
-
         Relation::morphMap([
             'articles' => \BBCMS\Models\Article::class,
             'categories' => \BBCMS\Models\Category::class,
