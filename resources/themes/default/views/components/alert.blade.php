ea54f4412984b168d710f8b5e9e0238345d32984
Рустам <rusiq@list.ru>
2019-01-11 23:58:59 +1000

    Update npm dependences, javascripts and css

--- resources/themes/default/views/components/alert.blade.php
+++ resources/themes/default/views/components/alert.blade.php
@@ -1,7 +1,11 @@
 <noscript>
     <div id="alert-message" class="alert alert-{{ $type or 'danger'}} alert-dismissible">
-        <button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">&times;</span></button>
-        @isset($title)<h5 class="alert-heading">@lang($title)</h5>@endisset
+        <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+            <span aria-hidden="true">&times;</span>
+        </button>
+        @isset($title)
+            <h5 class="alert-heading">@lang($title)</h5>
+        @endisset
         @lang($message)
     </div>
 </noscript>
@@ -9,7 +13,9 @@
 @push('scripts')
     <script>
         Notification.show({
-            @isset($title)title: '@lang($title)', @endisset
+            @isset($title)
+                title: '@lang($title)',
+            @endisset
             message: '@lang($message)',
             type: '{{ $type or 'error' }}'
         });
