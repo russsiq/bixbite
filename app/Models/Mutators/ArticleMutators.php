adb1ec756ae4e1d1b53cd386817689ea821f95f1
Рустам <rusiq@list.ru>
2019-02-10 08:49:19 +1000

    Поддержка shortcodes, включая доп.поля в контенте записи
    - [x] схема для доп.полей [[x_price]],
    - [x] схема для контейнера изображения [[picture_box_$id]]
    - [x] схема для ссылки на файл или изображение [[postfile_$id]] [[postimage_$id]]
    - [] создать раздел или настройки в записях для управления шоркодами

--- app/Models/Mutators/ArticleMutators.php
+++ app/Models/Mutators/ArticleMutators.php
@@ -24,6 +24,64 @@ trait ArticleMutators
         return $this->attributes['teaser'] ?? teaser($this->content, setting('articles.teaser_length', 150));
     }
 
+    /**
+     * Generate content with `shortcode` and `x_fields`.
+     *
+     * @return string
+     */
+    public function getContentAttribute()
+    {
+        if (is_null($content = $this->attributes['content'])) {
+            return null;
+        }
+
+        // Give shortcode from app settings. static::$shortcode ???
+        $shortcode = [
+            'phone' => '+7 (888) 888-88-88',
+            'address' => 'г.Негород, ул.Неулица, д.2019',
+        ];
+
+        // Give shortcode from extra fields. static::$x_shortcode ???
+        // $x_shortcode = $this->x_fields->keyBy('name')->toArray();
+        $x_shortcode = $this->x_fields->pluck('name')->toArray();
+
+        preg_match_all("/\[\[(.*?)\]\]/u", $content, $matches);
+
+        foreach ($matches[1] as $row) {
+            if (array_key_exists($row, $shortcode)) {
+                $content = str_ireplace('[[' . $row . ']]', $shortcode[$row], $content);
+            } elseif (in_array($row, $x_shortcode)) {
+                $content = str_ireplace('[[' . $row . ']]', $this->{$row}, $content);
+            } elseif (starts_with($row, 'picture_box_')) {
+                $id = (int) str_replace('picture_box_', '', $row);
+                if ($image = $this->images->where('id', $id)->first()) {
+                    $image = '<figure class="single_article__image">'
+                                .$image->getPictureBoxAttribute()
+                                .'<figcaption class="single_article_image__caption">'.$image->title.'</figcaption>'
+                            .'</figure>';
+                    $content = str_ireplace("[[picture_box_$id]]", $image, $content);
+                } else {
+                    $content = str_ireplace("[[picture_box_$id]]", '<code>IMG WITH ID #'.(int) $id.' IN THIS ARTICLE NOT FOUND.</code>', $content);
+                }
+            } elseif (starts_with($row, 'postfile_') or starts_with($row, 'postimage_')) {
+                $id = (int) str_ireplace(['postfile_', 'postimage_'], '', $row);
+                if ($file = $this->files->where('id', $id)->first()) {
+                    $content = str_ireplace([
+                            "[[postfile_$id]]",
+                            "[[postimage_$id]]"
+                        ], $file->url, $content);
+                } else {
+                    $content = str_ireplace([
+                        "[[postfile_$id]]",
+                        "[[postimage_$id]]"
+                    ], '<code>FILE WITH ID #'.(int) $id.' IN THIS ARTICLE NOT FOUND.</code>', $content);
+                }
+            }
+        }
+
+        return $content;
+    }
+
     public function getViewsAttribute()
     {
         return setting('articles.views_used', true) ? $this->attributes['views'] : null;
