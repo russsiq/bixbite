20c115108dfe6a604a39d0b098d6f9078668157c
Рустам <rusiq@list.ru>
2019-10-06 22:50:52 +1000

    Удалены безвозвратно из репозитория `config.settings`

--- config/settings/comments.php
+++
@@ -1,9 +0,0 @@
-<?php return array (
-  'regonly' => false,
-  'moderate' => false,
-  'nested' => true,
-  'widget_used' => true,
-  'widget_title' => 'Обсуждения',
-  'widget_count' => 8,
-  'widget_content_length' => 150,
-);
