6d93b2143d9affbdef989fb9eaf2afee2dbd9fd7
Рустам <rusiq@list.ru>
2019-11-06 15:53:05 +1000

    Update Setting.php

--- app/Models/Setting.php
+++ app/Models/Setting.php
@@ -88,13 +88,12 @@ class Setting extends BaseModel
                 return [
                     $setting->name => $setting->value
                 ];
-            })
-            ->toArray();
+            });
 
         // 6 Обновление настроек в директории `config/settings`.
         $path = config_path('settings');
         $file = $path.DS.$module->name.'.php';
-        $content = '<?php return '.var_export($updated, true).';';
+        $content = '<?php return '.var_export($updated->toArray(), true).';';
 
         if (! File::isDirectory($path)) {
             File::makeDirectory($path);
@@ -103,13 +102,35 @@ class Setting extends BaseModel
         File::put($file, $content, true);
 
         // 8 Записать переменные окружения в файл.
-        EnvManager::setMany($updated)->save();
+        self::pushToEnvFile($updated);
 
         // 9 Очистить и закешировать настройки.
         Artisan::call('config:cache');
 
         // 10 Возвращаем измененные настройки для модуля.
         return $settings;
+    }
+
+    /**
+     * Записать доступные переменные окружения в файл.
+     *
+     * @param  BaseCollection  $collection
+     * @return void
+     */
+    protected static function pushToEnvFile(BaseCollection $collection)
+    {
+        $collection = $collection->mapWithKeys(function ($value, $key) {
+                return [
+                    mb_strtoupper($key, 'UTF-8') => $value
+                ];
+            })
+            ->filter(function ($value, $key) {
+                return in_array($key, self::$_env);
+            });
+
+        if ($collection->isNotEmpty()) {
+            EnvManager::setMany($collection->toArray())->save();
+        }
     }
 
     public static function getAllowedFieldTypes()
