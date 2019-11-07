af9a39554ed04f98740b10868a3141a44ba1e744
Рустам <rusiq@list.ru>
2019-11-07 19:01:34 +1000

    Вынесены переменные ключей для Google reCaptcha в файл окружения

--- config/g_recaptcha.php
+++ config/g_recaptcha.php
@@ -4,8 +4,8 @@ return [
     'used' => true,
     'api_render' => 'https://www.google.com/recaptcha/api.js?render=',
     'api_verify' => 'https://www.google.com/recaptcha/api/siteverify',
-    
-    'site_key' => 'xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx',
-    'secret_key' => 'xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx',
+
+    'site_key' => env('GRECAPTCHA_SITE_KEY'),
+    'secret_key' => env('GRECAPTCHA_SECRET_KEY'),
     'score' => 0.5,
 ];
