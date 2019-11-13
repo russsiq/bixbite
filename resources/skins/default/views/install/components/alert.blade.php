07309e0d997d319686a5e579a0f8e92c92f5e7c3
Рустам <rusiq@list.ru>
2019-11-13 13:21:14 +1000

    Удалены шаблоны Установщика

--- resources/skins/default/views/install/components/alert.blade.php
+++
@@ -1,5 +0,0 @@
-<div id="alert-message" class="alert alert-{{ $type ?? 'danger'}} alert-dismissible">
-    <button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">&times;</span></button>
-    @isset($title)<h5 class="alert-heading">@lang($title)</h5>@endisset
-    @lang($message)
-</div>
--- resources/skins/default/views/install/components/alert_section.blade.php
+++
@@ -1,9 +0,0 @@
-<section class="alert-section">
-    <noscript><div class="alert alert-warning">@lang('msg.alert_noscript')</div></noscript>
-    @if ($errors->any())
-        @each('install.components.alert', $errors->all(), 'message')
-    @endif
-    @if ($message = session('status') ?? $message = session('message'))
-        @include('install.components.alert', ['type' => 'success', 'message' => trim($message)])
-    @endif
-</section>
