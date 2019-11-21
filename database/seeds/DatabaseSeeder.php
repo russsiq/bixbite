3c8f5bff1af81228d3c75c2bdce2b58ac43f8319
Рустам <rusiq@list.ru>
2019-11-22 05:41:45 +1000

    Удалены пережитки заполнителя

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -12,22 +12,13 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // Model::unguard();
-        
-        $baseDir = dirname(__FILE__);
-        
-        spl_autoload_register(function ($className) use ($baseDir) {
-            if (file_exists($file = $baseDir . '/database/seeds/' . $className . '.php')) {
-                include($file);
-                return true;
-            }
-        });
 
         $this->call([
             ModulesTableSeeder::class,
             PrivilegesTableSeeder::class,
             SettingsTableSeeder::class,
         ]);
-        
+
         // Model::reguard();
     }
 }
