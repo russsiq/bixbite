5931b1cbbb770f1f0fa8ddd80d6a540d35e91562
Рустам <rusiq@list.ru>
2019-11-23 05:20:47 +1000

    Удален помощник по минимальным требованиям

--- app/Support/helpers.php
+++ app/Support/helpers.php
@@ -291,50 +291,6 @@ if (! function_exists('html_secure')) {
     }
 }
 
-if (! function_exists('minreq')) {
-    /**
-     * Get info about minimum requirements.
-     *
-     * @param  string    $requirement
-     * @throws BadLogic  Unknown `requirement` argument.
-     * @return string
-     */
-    function minreq(string $requirement)
-    {
-        switch ($requirement) {
-            case 'php':
-                return version_compare(phpversion(), '7.2.2', '>=') ? phpversion() : false; break;
-            case 'pdo':
-                // return (extension_loaded('PDO') and extension_loaded('pdo_mysql') and class_exists('PDO')) ? \DB::select(\DB::raw("select version()"))[0]->{'version()'} : false; break;
-                return (extension_loaded('PDO') and class_exists('PDO')) ?? false; break;
-            case 'ssl':
-                return OPENSSL_VERSION_TEXT ?? false; break;
-            case 'gd':
-                return gd_info() ? gd_info()['GD Version'] : false; break;
-            case 'finfo':
-                return function_exists('finfo_open') ?? false; break; // PHP >= 7.2.0
-            case 'mb':
-                return extension_loaded('mbstring') ?? false; break;
-            case 'tokenizer':
-                return extension_loaded('tokenizer') ?? false; break;
-            case 'ctype':
-                return extension_loaded('ctype') ?? false; break;
-            case 'json':
-                return function_exists('json_encode') ?? false; break;
-            case 'zlib':
-                return (extension_loaded('zlib') and function_exists('ob_gzhandler')) ?? false; break;
-            case 'xml':
-                return function_exists('xml_parser_create') ?? false; break;
-            case 'curlLibrary':
-                return function_exists('curl_init') ?? false; break;
-            case 'zipLibrary':
-                return class_exists('ZipArchive') ?? false; break;
-        }
-
-        throw new BadLogic('Unknown requirement argument.');
-    }
-}
-
 if (! function_exists('pageinfo')) {
     /**
      * Working with global information about the current frontend page.
