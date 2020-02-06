719ae3d44846acaa92fb463a41d996d5eed0e0ee
Рустам <rusiq@list.ru>
2020-02-07 07:48:13 +1000

    Конфигурирование Мастера обновлений

--- config/assistant.php
+++ config/assistant.php
@@ -88,7 +88,71 @@ return [
 
     // Настройки Мастера обновлений.
     'updater' => [
-        //
+        // Путь к временной папке для загрузки обновления из репозитория.
+        'download_path' => env('ASSISTANT_DOWNLOAD_PATH', storage_path('tmp')),
+
+        // Указанные папки будут пропущены во время процесса обновления.
+        'exclude_directories' => [
+            'bootstrap/cache',
+            'config/settings',
+            'node_modules',
+            'public',
+            'storage',
+            'vendor', // Перед релизом CMS закомментировать.
+
+        ],
+
+        // Указанные файлы расположены в корне приложения и будут обновлены.
+        // А другие файлы (например, `webpack.mix.js`)
+        // будут исключены из списка обновляемых.
+        'allowed_files' => [
+            'artisan',
+            'composer.json',
+            'LICENSE',
+            'package.json',
+            'readme.md',
+            // 'webpack.mix.js',
+
+        ],
+
+        // Настройки файла версионирования.
+        'version_file' => [
+            // Имя файла версионирования со сведениями о релизе.
+            'filename' => 'assistant-new-version',
+
+            // Время хранения файла версионирования в секундах.
+            // По факту частота проверки наличия обновлений.
+            'store_time' => 24 * 60 * 60,
+
+        ],
+
+        // Формат строки имени архива, описывающий версию приложения.
+        // Например, имя вложения: `app_name-v1.0.0.zip`.
+        'version_format' => env('ASSISTANT_VERSION_FORMAT', 'bixbite-v__VERSION__.zip'),
+
+        // Драйвер, используемый по умолчанию.
+        // Поддерживаемые типы: `github`.
+        'driver' => 'github',
+
+        // Настройки драйверов.
+        'github' => [
+            'driver' => 'github',
+            'endpoint' => 'https://api.github.com/repos/russsiq/bixbite/releases/latest',
+            'version_key' => 'tag_name',
+            'source_key' => 'zipball_url',
+
+            // Настройки HTTP клиента.
+            'guzzle' => [
+                // 'base_url' => 'https://api.github.com',
+                'headers' => [
+                    'Authorization' => 'Bearer '.env('ASSISTANT_GITHUB_ACCESS_TOKEN'),
+
+                ],
+
+            ],
+
+        ],
+
     ],
 
 ];
