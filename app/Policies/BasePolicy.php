869549c988ce4eb68889115717af009338995bf9
Рустам <rusiq@list.ru>
2020-02-17 21:59:37 +1000

    Комментирование + Форматирование Базового класса политик

--- app/Policies/BasePolicy.php
+++ app/Policies/BasePolicy.php
@@ -2,21 +2,33 @@
 
 namespace BBCMS\Policies;
 
+// Исключения.
 use BadMethodCallException;
 
+// Сторонние зависимости.
 use BBCMS\Models\User;
-
 use Illuminate\Auth\Access\HandlesAuthorization;
 
+/**
+ * Базовый класс политик,
+ * описывающий привилегии пользователей сайта.
+ */
 class BasePolicy
 {
     use HandlesAuthorization;
 
-    public function __call($method, $parameters)
+    /**
+     * Проксирование политик, неопределенных в дочерних классах.
+     * Разрешен полный доступ для собственника сайта.
+     * @param  string  $method
+     * @param  array  $parameters
+     * @return bool
+     *
+     * @throws BadMethodCallException
+     */
+    public function __call(string $method, array $parameters): bool
     {
-        // throw new \Exception(...$parameters);
-
-        $user = $parameters[0];
+        $user = array_shift($parameters);
 
         if ($user instanceof User) {
             return $user->hasRole('owner');
