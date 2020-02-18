5b8597fe5092b22f6de2c36ecb53048203b1ec6d
Рустам <rusiq@list.ru>
2020-02-18 16:48:26 +1000

    Удален устаревший код в базовой модели

--- app/Models/BaseModel.php
+++ app/Models/BaseModel.php
@@ -7,26 +7,4 @@ use Illuminate\Database\Eloquent\Model;
 abstract class BaseModel extends Model
 {
 
-    /**
-     * The "booting" method of the model.
-     *
-     * @return void
-     */
-    protected static function boot()
-    {
-        parent::boot();
-    }
-
-    // /**
-    //  * Get the default foreign key name for the model.
-    //  *
-    //  * @return string
-    //  */
-    // public function getForeignKey()
-    // {
-    //     parent::getForeignKey();
-    //
-    //     return env('DB_PREFIX') . snake_case(class_basename($this)) . '_' . $this->getKeyName();  // \DB::getTablePrefix()
-    // }
-
 }
