e1e6c6b8a3fcb3db55ddaf3e8d33dba3424b3b59
Рустам <rusiq@list.ru>
2020-02-19 19:28:18 +1000

    Update Fileable.php
    Комментирование + Форматирование

--- app/Models/Relations/Fileable.php
+++ app/Models/Relations/Fileable.php
@@ -2,42 +2,37 @@
 
 namespace BBCMS\Models\Relations;
 
+// Сторонние зависимости.
 use BBCMS\Models\File;
+use Illuminate\Database\Eloquent\Collection;
+use Illuminate\Database\Eloquent\Relations\MorphMany;
 
 trait Fileable
 {
-    public function files()
+    /**
+     * Получить все файлы, прикрепленные к модели.
+     * @return MorphMany
+     */
+    public function files(): MorphMany
     {
         return $this->morphMany(File::class, 'attachment', 'attachment_type', 'attachment_id', 'id');
     }
 
-    // public function image() {
-    //     return $this->hasOne(File::class, 'attachment_id', 'id')
-    //         ->where('attachment_type', $this->getMorphClass())
-    //         ->where('type', 'image');
-    // }
-
-    public function getImageAttribute()
-    {
-        return $this->images->where('id', $this->image_id)->first();
-
-        // ->with([
-        //     'files' => function ($query) {
-        //         $query->select([
-        //             'files.id', 'files.disk', 'files.type',
-        //             'files.category', 'files.name', 'files.extension',
-        //             'files.attachment_type','files.attachment_id'
-        //         ])
-        //         ->join('categories', function ($join) {
-        //             $join->on('files.id', '=', 'categories.image_id');
-        //         })
-        //         ->where('type', 'image');
-        //     },
-        // ])
-    }
-
-    public function getImagesAttribute()
+    /**
+     * Получить коллекцию изображений, прикрепленных к модели.
+     * @return Collection
+     */
+    public function getImagesAttribute(): Collection
     {
         return $this->files->where('type', 'image');
+    }
+
+    /**
+     * Получить основное изображение, закрепленное за сущностью.
+     * @return File
+     */
+    public function getImageAttribute(): ?File
+    {
+        return $this->images->where('id', $this->image_id)->first();
     }
 }
