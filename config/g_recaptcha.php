fdde296125e9fe64f9b46a97ba69928d065b4929
Рустам <rusiq@list.ru>
2020-03-02 06:47:39 +1000

    Актуализация файла конфигураций валидатора капчи

--- config/g_recaptcha.php
+++ config/g_recaptcha.php
@@ -1,11 +1,44 @@
 <?php
 
 return [
-    'used' => true,
-    'api_render' => 'https://www.google.com/recaptcha/api.js?render=',
-    'api_verify' => 'https://www.google.com/recaptcha/api/siteverify',
+    'used' => false,
 
-    'site_key' => env('GRECAPTCHA_SITE_KEY'),
-    'secret_key' => env('GRECAPTCHA_SECRET_KEY'),
-    'score' => 0.5,
+    // Логирование событий.
+    'log_events' => env('GRECAPTCHA_LOG_EVENTS', false),
+
+    // Драйвер, используемый по умолчанию.
+    // Поддерживаемые типы: `nullable`, `google_v3`.
+    'driver' => env('GRECAPTCHA_DRIVER', 'google_v3'),
+
+    // Настройки драйверов.
+    'drivers' => [
+        'nullable' => [
+            'driver' => 'nullable',
+
+        ],
+
+        // Google reCAPTCHA v3.
+        // https://developers.google.com/recaptcha/docs/v3
+        'google_v3' => [
+            'driver' => 'google_v3',
+
+            // Параметры драйвера.
+            'api_render' => 'https://www.google.com/recaptcha/api.js?render=',
+            'api_verify' => 'https://www.google.com/recaptcha/api/siteverify',
+
+            'site_key' => env('GRECAPTCHA_SITE_KEY'),
+            'secret_key' => env('GRECAPTCHA_SECRET_KEY'),
+
+            'score' => 0.5,
+
+            // Настройки HTTP клиента.
+            'guzzle' => [
+                // 'base_uri' => 'https://www.google.com/recaptcha/',
+
+            ],
+
+        ],
+
+
+    ],
 ];
