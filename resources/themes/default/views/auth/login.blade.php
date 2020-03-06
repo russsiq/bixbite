734f3cf77cc6a07ea5c23854961b8628679e02c3
Рустам <rusiq@list.ru>
2020-03-07 09:30:09 +1000

    Update login.blade.php

--- resources/themes/default/views/auth/login.blade.php
+++ resources/themes/default/views/auth/login.blade.php
@@ -26,7 +26,7 @@
                     @endif
                 </div>
 
-                <div class="form-group row has-error">
+                <div class="form-group row">
                     <label for="password" class="col-md-4 col-form-label">@lang('auth.password')</label>
                     <div class="col-md-6">
                         <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required>
