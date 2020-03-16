84e1baf8fa146ec454e3ad09e32fb92ce7920a90
Рустам <rusiq@list.ru>
2020-03-16 12:48:12 +1000

    Удалены устаревшие куски комментариев

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -29,58 +29,6 @@ class Handler extends ExceptionHandler
         'password_confirmation',
     ];
 
-    // /**
-    //  * Report or log an exception.
-    //  *
-    //  * This is a great spot to send exceptions to Sentry, Bugsnag, etc.
-    //  *
-    //  * @param  \Exception  $e
-    //  * @return void
-    //  */
-    // public function report(Exception $e)
-    // {
-    //     parent::report($e);
-    // }
-    //
-    // /**
-    //  * Render an exception into an HTTP response.
-    //  *
-    //  * @param  \Illuminate\Http\Request  $request
-    //  * @param  \Exception  $e
-    //  * @return \Illuminate\Http\Response
-    //  */
-    // public function render($request, \Exception $e)
-    // {
-    //     if ($e instanceof ModelNotFoundException) {
-    //         // Методы findOrFail() и firstOrFail()
-    //         // Ваша логика для ненайденной модели...
-    //     }
-    //     if ($this->isHttpException($e)) {
-    //         return $this->renderHttpException($e);
-    //     }
-    //
-    //     return parent::render($request, $e);
-    // }
-    //
-    // /**
-    //  * Render the given HttpException.
-    //  *
-    //  * @param  \Symfony\Component\HttpKernel\Exception\HttpException  $e
-    //  * @return \Illuminate\Http\Response
-    //  */
-    // protected function renderHttpException(HttpException $e)
-    // {
-    //     if (view()->exists('errors')) {
-    //         return response()->view('errors', [
-    //         'title' => $e->getStatusCode(),
-    //         'message' => $e->getMessage()
-    //         ],
-    //         $e->getStatusCode());
-    //     }
-    //
-    //     return parent::renderHttpException($e);
-    // }
-
     /**
      * Register the error template hint paths.
      *
