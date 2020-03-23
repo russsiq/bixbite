ac8e07e8ff0eaa378ee1be034517ac4b10978668
Рустам <rusiq@list.ru>
2020-03-24 09:13:29 +1000

    Удален избыточный шаблон капчи

--- resources/themes/default/views/components/partials/captcha.blade.php
+++
@@ -1,5 +0,0 @@
-<div class="input-group">
-    <input type="number" name="captcha" value="" class="form-control" placeholder="@lang('auth.captcha')" step="1" min="1000" max="9999" required />
-    <span class="input-group-addon p-0"><img id="img_captcha" src="{{ route('captcha.url', ['rand' => $captcha_rand]) }}" alt="captcha" class="captcha" /></span>
-</div>
-@if ($errors->has('captcha'))<span class="invalid-feedback d-block">{{ $errors->first('captcha') }}</span>@endif
