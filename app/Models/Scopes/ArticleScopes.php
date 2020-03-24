e3b85a822f387200a468df984f3adee3820c7d48
Рустам <rusiq@list.ru>
2020-03-24 13:58:32 +1000

    Форматирование

--- app/Models/Scopes/ArticleScopes.php
+++ app/Models/Scopes/ArticleScopes.php
@@ -11,39 +11,41 @@ trait ArticleScopes
 {
     /**
      * Фильтрация записей по часто используемым критериям.
-     * @param  Builder  $query
+     * @param  Builder  $builder
      * @param  array  $filters
      * @return void
      */
-    public function scopeFilter(Builder $query, array $filters): void
+    public function scopeFilter(Builder $builder, array $filters): void
     {
-        $query->when($filters['month'], function(Builder $query, string $month) {
-            $query->whereMonth('created_at', Carbon::parse($month)->month);
+        $builder->when($filters['month'], function(Builder $builder, string $month) {
+            $builder->whereMonth('created_at', Carbon::parse($month)->month);
         })
-        ->when($filters['year'], function(Builder $query, int $year) {
-            $query->whereYear('created_at', $year);
+        ->when($filters['year'], function(Builder $builder, int $year) {
+            $builder->whereYear('created_at', $year);
         })
-        ->when($filters['user_id'], function(Builder $query, int $user_id) {
-            $query->where('user_id', $user_id);
+        ->when($filters['user_id'], function(Builder $builder, int $user_id) {
+            $builder->where('user_id', $user_id);
         });
     }
 
     /**
      * [scopeFullArticle description]
-     * @param  Builder  $query
+     * @param  Builder  $builder
      * @param  array  $filters
      * @return void
      */
-    public function scopeFullArticle(Builder $query, int $id): void
+    public function scopeFullArticle(Builder $builder, int $id): void
     {
-        $query->with([
+        $builder->with([
                 // 'categories:categories.id,categories.title,categories.slug',
                 'user:users.id,users.name,users.email,users.avatar',
                 'files',
+
             ])
             ->withCount([
                 'comments',
                 'tags',
+
             ])
             ->where('articles.id', $id)
             ->published();
@@ -51,11 +53,11 @@ trait ArticleScopes
 
     /**
      * [scopeCachedFullArticleWithRelation description]
-     * @param  Builder  $query
-     * @param  integer  $id
+     * @param  Builder  $builder
+     * @param  int  $id
      * @return Article
      */
-    public function scopeCachedFullArticleWithRelation(Builder $query, int $id): Article
+    public function scopeCachedFullArticleWithRelation(Builder $builder, int $id): Article
     {
         $article = cache()->remember('articles-single-'.$id, setting('articles.cache_used', 1440) * 60, function () use ($id) {
             $article = $this->fullArticle($id)->firstOrFail();
@@ -77,40 +79,47 @@ trait ArticleScopes
 
     /**
      * [scopePublished description]
-     * @param  Builder  $query
+     * @param  Builder  $builder
      * @return void
      */
-    public function scopePublished(Builder $query): void
+    public function scopePublished(Builder $builder): void
     {
-        $query->where('articles.state', 'published');
+        $builder->where('articles.state', 'published');
     }
 
     /**
      * [scopeSearchByKeyword description]
-     * @param  Builder  $query
+     * @param  Builder  $builder
      * @param  string|null  $keyword
      * @return void
      */
-    public function scopeSearchByKeyword(Builder $query, ?string $keyword): void // ToDo Add new xfields
+    public function scopeSearchByKeyword(Builder $builder, ?string $keyword): void // ToDo Add new xfields
     {
-        $query->when($keyword, function(Builder $query, string $keyword) {
-            $query->where('title', 'like', '%' . $keyword . '%')
+        $builder->when($keyword, function(Builder $builder, string $keyword) {
+            $builder->where('title', 'like', '%' . $keyword . '%')
                 ->orWhere('content', 'like', '%' . $keyword . '%');
         });
     }
 
     /**
      * [scopeShortArticle description]
-     * @param  Builder  $query
+     * @param  Builder  $builder
      * @return void
      */
-    public function scopeShortArticle(Builder $query): void
+    public function scopeShortArticle(Builder $builder): void
     {
-        $query->select([
-                'articles.id','articles.user_id','articles.image_id',
-                'articles.slug','articles.title','articles.teaser',
-                'articles.created_at','articles.updated_at',
-                'articles.views', 'articles.state',
+        $builder->addSelect([
+                'articles.id',
+                'articles.user_id',
+                'articles.image_id',
+                'articles.slug',
+                'articles.title',
+                'articles.teaser',
+                'articles.created_at',
+                'articles.updated_at',
+                'articles.views',
+                'articles.state',
+
             ])
             ->addSelect(
                 $this->x_fields->pluck('name')->all()
@@ -119,16 +128,23 @@ trait ArticleScopes
                 // 'categories:categories.id,categories.slug,categories.title',
                 'user:users.id,users.name', // users.email,users.avatar',
                 'files' => function ($query) {
-                    $query->select([
-                        'files.id', 'files.disk', 'files.type',
-                        'files.category', 'files.name', 'files.extension',
+                    $query->addSelect([
+                        'files.id',
+                        'files.disk',
+                        'files.type',
+                        'files.category',
+                        'files.name',
+                        'files.extension',
                         'files.title',
-                        'files.attachment_type', 'files.attachment_id'
+                        'files.attachment_type',
+                        'files.attachment_id',
+
                     ]);
                 },
             ])
             ->withCount([
                 'comments',
+
             ])
             ->published();
     }
