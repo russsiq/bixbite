69adb9a916871604f1afaba380d89cc62ba0f984
Рустам <rusiq@list.ru>
2020-03-27 14:22:25 +1000

    Изменены маршруты RSS лент

--- routes/web/rss.php
+++ routes/web/rss.php
@@ -4,9 +4,9 @@ use Illuminate\Support\Facades\Route;
 
 // Карты сайта. https://www.sitemaps.org/protocol.html.
 Route::get('sitemap.xml', 'SitemapController@index')->name('sitemap.xml');
-Route::get('sitemap/home.xml', 'SitemapController@home')->name('sitemap.home.xml');
-Route::get('sitemap/articles.xml', 'SitemapController@articles')->name('sitemap.articles.xml');
-Route::get('sitemap/categories.xml', 'SitemapController@categories')->name('sitemap.categories.xml');
+Route::get('sitemap-home.xml', 'SitemapController@home')->name('sitemap-home.xml');
+Route::get('sitemap-articles.xml', 'SitemapController@articles')->name('sitemap-articles.xml');
+Route::get('sitemap-categories.xml', 'SitemapController@categories')->name('sitemap-categories.xml');
 
 // Турбо страницы для Yandex. https://yandex.ru/support/webmaster/turbo/feed.html.
-Route::get('amp/articles.xml', 'AmpController')->name('amp.articles.xml');
+Route::get('amp-articles.xml', 'AmpController')->name('amp-articles.xml');
