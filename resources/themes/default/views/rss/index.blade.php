2931f685f6fe2894bced69aafc6500937ddb2699
Рустам <rusiq@list.ru>
2020-03-27 14:23:39 +1000

    Изменены ссылки в основной карте сайта в соответствии с маршрутами

--- resources/themes/default/views/rss/index.blade.php
+++ resources/themes/default/views/rss/index.blade.php
@@ -1,14 +1,14 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <sitemapindex xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
     <sitemap>
-        <loc>{{ route('sitemap.home.xml') }}</loc>
+        <loc>{{ route('sitemap-home.xml') }}</loc>
     </sitemap>
     <sitemap>
-        <loc>{{ route('sitemap.categories.xml') }}</loc>
+        <loc>{{ route('sitemap-categories.xml') }}</loc>
         <lastmod>{{ ($categories->updated_at ?? $categories->created_at)->tz('UTC')->toAtomString() }}</lastmod>
     </sitemap>
     <sitemap>
-        <loc>{{ route('sitemap.articles.xml') }}</loc>
+        <loc>{{ route('sitemap-articles.xml') }}</loc>
         <lastmod>{{ ($articles->updated_at ?? $articles->created_at)->tz('UTC')->toAtomString() }}</lastmod>
     </sitemap>
 </sitemapindex>
