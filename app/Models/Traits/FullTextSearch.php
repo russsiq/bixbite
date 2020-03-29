a0d384476efed598f380a3566cfebaeeba5236c7
Рустам <rusiq@list.ru>
2020-03-29 13:50:09 +1000

    Update FullTextSearch.php

--- app/Models/Traits/FullTextSearch.php
+++ app/Models/Traits/FullTextSearch.php
@@ -4,6 +4,8 @@ namespace App\Models\Traits;
 
 // Сторонние зависимости.
 use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Support\Str;
+use Illuminate\Support\Collection;
 
 /**
  * Source https://arianacosta.com/php/laravel/tutorial-full-text-search-laravel-5/.
@@ -11,56 +13,63 @@ use Illuminate\Database\Eloquent\Builder;
 trait FullTextSearch
 {
     /**
-     * Scope a query that matches a full text search of term.
+     * Диапазон запроса полнотекстового поиска.
      * @param  Builder  $query
      * @param  string  $term
      * @return void
      */
     public function scopeSearch(Builder $query, string $term): void
     {
-        $columns = implode(',', $this->searchable);
+        $columns = $this->fullTextColumns();
+        $searchTerm = $this->fullTextWildcards($term);
 
-        $query->whereRaw(
-            "MATCH ({$columns}) AGAINST (? IN BOOLEAN MODE)",
-            $this->fullTextWildcards($term)
-        );
-
-        // $term = $this->fullTextWildcards($term);
-        //
-        // $query
-        //     ->selectRaw('MATCH (title, content) AGAINST (? in boolean mode) as REL', [$term])
-        //     ->whereRaw('MATCH (title, content) AGAINST (? in boolean mode)' , $term)
-        //     ->orderBy('REL', 'desc')
-        //     ;
+        $query->selectRaw(
+                "MATCH ({$columns}) AGAINST (? in boolean mode) as REL", [
+                $searchTerm
+            ])
+            ->whereRaw(
+                "MATCH ({$columns}) AGAINST (? in boolean mode)", [
+                $searchTerm
+            ])
+            ->orderBy('REL', 'desc');
     }
 
     /**
-     * Replaces spaces with full text search wildcards
+     * Подготовить столбцы для полнотекстового поиска.
+     * @return string
+     */
+    protected function fullTextColumns(): string
+    {
+        $table = $this->getTable();
+        $grammar = $this->getQuery()->getGrammar();
+
+        return collect($this->searchable)
+            ->map(function (string $column) use ($grammar, $table) {
+                return $grammar->wrap($table.'.'.$column);
+            })
+            ->implode(',');
+    }
+
+    /**
+     * Подготовить строку для полнотекстового поиска.
      * @param  string  $term
      * @return string
      */
     protected function fullTextWildcards(string $term): string
     {
-        // removing symbols used by MySQL
+        // Удаление символов, используемых MySQL.
         $reservedSymbols = ['-', '+', '<', '>', '@', '(', ')', '~'];
 
-        $term = str_replace($reservedSymbols, '', $term);
-
-        $words = explode(' ', $term);
-
-        foreach ($words as $key => $word) {
-            /*
-             * applying + operator (required word) only big words
-             * because smaller ones are not indexed by mysql
-             */
-            if (strlen($word) >= 3) {
-                // $words[$key] = '+' . $word . '*';
-                $words[$key] = $word.'*';
-            }
-        }
-
-        $searchTerm = implode(' ', $words);
-
-        return $searchTerm;
+        return Str::of($term)
+            ->replace($reservedSymbols, '')
+            ->explode(' ')
+            ->filter(function (string $word) {
+                // Исключаем слова короче трех символов.
+                return strlen(trim($word)) >= 3;
+            })
+            ->map(function (string $word) {
+                return $word; // '+' . $word . '*';
+            })
+            ->implode(' ');
     }
 }
