b8793d28f953839266fe1b02381e1984fa77fc4f
Рустам <rusiq@list.ru>
2020-03-30 23:38:27 +1000

    Update sidebar_right.blade.php

--- resources/themes/default/views/components/sidebar_right.blade.php
+++ resources/themes/default/views/components/sidebar_right.blade.php
@@ -2,20 +2,20 @@
     <x-widgets.articles-featured :parameters="[
         'is_active' => true,
         'limit' => 5,
-        // 'cache_time' => 0,
-        // 'sub_day' => 1,
-        // 'order_by' => 'title',
-        // 'direction' => 'asc',
-        // 'id' => [6,8],
-        // 'user_id' => [1,2],
-        // 'categories' => [1,3,4],
-        // 'tags' => ['бизнес','политика'],
+        {{-- 'cache_time' => 0, --}}
+        {{-- 'sub_day' => 1, --}}
+        {{-- 'order_by' => 'title', --}}
+        {{-- 'direction' => 'asc', --}}
+        {{-- 'id' => [6,8], --}}
+        {{-- 'user_id' => [1,2], --}}
+        {{-- 'categories' => [1,3,4], --}}
+        {{-- 'tags' => ['бизнес','политика'], --}}
 
     ]" />
 
     @if (setting('comments.widget_used', true))
         <x-widgets.comments-latest :parameters="[
-            // 'cache_time' => 0,
+            {{-- 'cache_time' => 0, --}}
 
         ]" />
     @endif
