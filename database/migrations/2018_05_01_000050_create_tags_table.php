986a341fc669427f9c1206c1755bfbb53fa496ba
Рустам <rusiq@list.ru>
2020-03-30 23:15:54 +1000

    Добавлены временные метки в миграцию таблицы `tags`
    ... позволит сортировать по этим полям в виджете Облако меток

--- database/migrations/2018_05_01_000050_create_tags_table.php
+++ database/migrations/2018_05_01_000050_create_tags_table.php
@@ -16,6 +16,9 @@ class CreateTagsTable extends Migration
         Schema::create('tags', function (Blueprint $table) {
             $table->id();
             $table->string('title')->unique();
+
+            // Timestamps.
+            $table->timestamps();
         });
 
     }
