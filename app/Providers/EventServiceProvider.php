7b7a39a29577b4dc27755a87a66ccbb225e1fda9
Рустам <rusiq@list.ru>
2020-04-05 14:46:51 +1000

    Комментирование

--- app/Providers/EventServiceProvider.php
+++ app/Providers/EventServiceProvider.php
@@ -14,7 +14,7 @@ use Illuminate\Foundation\Support\Providers\EventServiceProvider as ServiceProvi
 class EventServiceProvider extends ServiceProvider
 {
     /**
-     * The event listener mappings for the application.
+     * Сопоставление событий и слушателей приложения.
      * @var array
      */
     protected $listen = [
