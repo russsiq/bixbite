b818b580627766375bbf733c86ead347c95dde8c
Рустам <rusiq@list.ru>
2020-05-27 23:09:47 +1000

    [Тесты] Тестирование метода `shouldRender` компонента `ConsentCookie`

---
+++ tests/Feature/App/View/Components/ConsentCookieTest.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace Tests\Feature\App\View\Components;
+
+// Тестируемый класс.
+use App\View\Components\ConsentCookie;
+
+// Сторонние зависимости.
+use Tests\TestCase;
+
+/**
+ * @coversDefaultClass \App\View\Components\ConsentCookie
+ */
+class ConsentCookieTest extends TestCase
+{
+    /**
+     * @test
+     * @covers ::shouldRender
+     *
+     * Пользователь можеть видеть панель об использовании кук на сайте.
+     * @return void
+     */
+    public function testUserCanViewConsent(): void
+    {
+        $this->get(route('home'));
+
+        $component = app(ConsentCookie::class);
+
+        $this->assertTrue($component->shouldRender());
+    }
+
+    /**
+     * @test
+     * @covers ::shouldRender
+     *
+     * Пользователь не можеть видеть панель об использовании кук на сайте,
+     * когда он уже дал свое согласие на их обработку.
+     * @return void
+     */
+    public function testUserCanNotViewConsentWhenAccepted(): void
+    {
+        $this->disableCookieEncryption()
+            ->withCookie(
+                ConsentCookie::ACCEPTED_NAME,
+                ConsentCookie::ACCEPTED_VALUE
+            )
+            ->get(route('home'));
+
+        $component = app(ConsentCookie::class);
+
+        $this->assertFalse($component->shouldRender());
+    }
+}
