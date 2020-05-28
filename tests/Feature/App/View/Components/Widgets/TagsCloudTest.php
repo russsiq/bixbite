c96528b267f1b67209bf72b118be9c885d02e8d3
Рустам <rusiq@list.ru>
2020-05-28 16:28:00 +1000

    [Тесты] Общий тест для виджета `Облако меток`

---
+++ tests/Feature/App/View/Components/Widgets/TagsCloudTest.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace Tests\Feature\App\View\Components\Widgets;
+
+// Тестируемый класс.
+use App\View\Components\Widgets\TagsCloud;
+
+// Сторонние зависимости.
+use App\Models\Tag;
+use Illuminate\Contracts\View\View as ViewContract;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Tests\TestCase;
+
+/**
+ * @coversDefaultClass \App\View\Components\Widgets\TagsCloud
+ */
+class TagsCloudTest extends TestCase
+{
+    use RefreshDatabase;
+
+    public function setUp(): void
+    {
+        $this->afterApplicationCreated(function () {
+            // Добавляем пространство имен шаблонов активной темы.
+            $this->app->view->addLocation(theme_path('views'));
+        });
+
+        parent::setUp();
+    }
+
+    /**
+     * @test
+     *
+     * Пользователь можеть наблюдать пустое облако тегов.
+     * @return void
+     */
+    public function testUserCanViewEmptyTagsCloud(): void
+    {
+        $component = $this->app->make(TagsCloud::class);
+
+        $component->disableCache()
+            ->resolveView()
+            ->with($component->data())
+            ->render(function (ViewContract $view, string $contents) {
+                $variables = $view->getData();
+
+                // $this->assertArrayHasKey('tags', $variables);
+                $tags = $variables['tags'];
+                $this->assertTrue($tags()->isEmpty());
+            });
+    }
+}
