5b933da1d2806d90bdd63f6f900b265a9c297652
Рустам <rusiq@list.ru>
2020-05-31 20:46:50 +1000

    Update ArticlesControllerTest.php
    Уточнены моменты с путаницей в драйверах аутентификации

--- tests/Feature/App/Http/Controllers/Api/V1/ArticlesControllerTest.php
+++ tests/Feature/App/Http/Controllers/Api/V1/ArticlesControllerTest.php
@@ -16,6 +16,18 @@ use Tests\TestCase;
 
 /**
  * @coversDefaultClass \App\Http\Controllers\Api\V1\ArticlesController
+ *
+ * @NB Приложение использует двойную аутентификацию
+ * для доступа к API-ресурсам.
+ * Тестируя только api-драйвер аутентификации,
+ * мы не сможем сымитировать действительность.
+ *
+ * @NB Метод `actingAs` ведёт себя по-разному с `null` и `web` драйверами,
+ * хотя последний должен использоваться как драйвер по-умолчанию.
+ *
+ * @NB При указании драйвера `api`
+ * заголовок `Authorization` со значением `Bearer`
+ * непредсказуемо влияет на ответ.
  */
 class ArticlesControllerTest extends TestCase
 {
@@ -25,55 +37,78 @@ class ArticlesControllerTest extends TestCase
      * @test
      * @covers ::index
      *
-     * Функциональное покрытие метода `index` api-контроллера Записей.
+     * Ошибка аутентификации при просмотре списка записей гостем.
      * @return void
      */
-    public function testArticlesIndex(): void
+    public function testAuthenticationFailedWhileGuestListingArticles(): void
     {
-        $routeName = 'api.articles.index';
-
-        // Ошибка аутентификации при просмотре списка записей гостем.
-        $this->assertGuest()
-            ->getJson(route($routeName))
+        $this->getJson(route('api.articles.index'))
             ->assertStatus(JsonResponse::HTTP_UNAUTHORIZED);
+    }
 
-        // Ошибка аутентификации при просмотре списка записей пользователем.
+    /**
+     * @test
+     * @covers ::index
+     *
+     * Ошибка аутентификации при просмотре списка записей пользователем.
+     * @return void
+     */
+    public function testAuthenticationFailedWhileUserListingArticles(): void
+    {
         $this->actingAs($user = $this->createImprovisedUser())
-            ->assertAuthenticated('api')
-            ->getJson(route($routeName))
-            ->assertStatus(JsonResponse::HTTP_FORBIDDEN);
+            ->getJson(route('api.articles.index'))
+            ->assertStatus(JsonResponse::HTTP_UNAUTHORIZED);
+    }
 
-        // Доступ запрещен при просмотре списка записей пользователем.
-        $this->withHeaders([
+    /**
+     * @test
+     * @covers ::index
+     *
+     * Доступ запрещен при просмотре списка записей пользователем.
+     * @return void
+     */
+    public function testForbiddenWhileUserListingArticles(): void
+    {
+        $this->actingAs($user = $this->createImprovisedUser())
+            ->withHeaders([
                 'Authorization' => 'Bearer '.$user->generateApiToken(),
             ])
-            ->getJson(route($routeName))
+            ->getJson(route('api.articles.index'))
             ->assertStatus(JsonResponse::HTTP_FORBIDDEN);
+    }
 
-        /**
-         * Прорабатываем варианты доступа собственника сайта к списку записей.
-         */
+    /**
+     * @test
+     * @covers ::index
+     *
+     * Собственник сайта получает пустой список записей.
+     * @return void
+     */
+    public function testOwnerRecivesEmptyArticlesList(): void
+    {
+        $this->actingAsOwner()
+            ->getJson(route('api.articles.index'))
+            ->assertStatus(JsonResponse::HTTP_PARTIAL_CONTENT);
+    }
+
+    /**
+     * @test
+     * @covers ::index
+     *
+     * Собственник сайта получает список записей.
+     * @return void
+     */
+    public function testOwnerRecivesArticles(): void
+    {
         $articles = factory(Article::class, $articlesCount = mt_rand(4, 12))
             ->create();
 
-        // Собственник сайта получает пустой список записей.
-        $this->actingAs($owner = $this->createImprovisedUser('owner'))
-            ->assertAuthenticated('api')
-            ->getJson(route($routeName))
-            ->assertStatus(JsonResponse::HTTP_PARTIAL_CONTENT);
-
-        // Собственник сайта получает пустой список записей.
-        $this->withHeaders([
-                'Authorization' => 'Bearer '.$owner->generateApiToken(),
-            ])
-            ->getJson(route($routeName))
-            ->assertStatus(JsonResponse::HTTP_PARTIAL_CONTENT);
-
-        $this->getJson(route($routeName, [
+        $this->actingAsOwner()
+            ->getJson(route('api.articles.index', [
                 'limit' => $articlesCount
             ]))
-            ->assertJsonCount($articlesCount, 'data')
             ->assertStatus(JsonResponse::HTTP_PARTIAL_CONTENT)
+            ->assertJsonCount($articlesCount, 'data')
             ->assertJsonStructure([
                 'data' => [
                     [
@@ -109,7 +144,7 @@ class ArticlesControllerTest extends TestCase
     {
         $this->actingAs($user = $this->createImprovisedUser())
             ->postJson(route('api.articles.store'))
-            ->assertStatus(JsonResponse::HTTP_FORBIDDEN);
+            ->assertStatus(JsonResponse::HTTP_UNAUTHORIZED);
     }
 
     /**
@@ -127,6 +162,20 @@ class ArticlesControllerTest extends TestCase
             ])
             ->postJson(route('api.articles.store'))
             ->assertStatus(JsonResponse::HTTP_FORBIDDEN);
+    }
+
+    /**
+     * @test
+     * @covers ::store
+     *
+     * Ошибка аутентификации при создании записи собственником сайта.
+     * @return void
+     */
+    public function testAuthenticationFailedWhileOwnerCreateArticle(): void
+    {
+        $this->actingAs($owner = $this->createImprovisedUser('owner'))
+            ->postJson(route('api.articles.store'))
+            ->assertStatus(JsonResponse::HTTP_UNAUTHORIZED);
     }
 
     /**
@@ -188,6 +237,12 @@ class ArticlesControllerTest extends TestCase
     public function testForbiddenWhileUserUpdateArticle(): void
     {
         $article = factory(Article::class)->create();
+
+        $this->actingAs($user = $this->createImprovisedUser())
+            ->putJson(route('api.articles.update', $article->id), [
+                'title' => 'New title'
+            ])
+            ->assertStatus(JsonResponse::HTTP_UNAUTHORIZED);
 
         $this->actingAs($user = $this->createImprovisedUser())
             ->withHeaders([
@@ -262,14 +317,12 @@ class ArticlesControllerTest extends TestCase
      */
     public function testForbiddenWhileUserMassUpdateArticle(): void
     {
-        $articles = factory(Article::class, 3)->create();
-
         $this->actingAs($user = $this->createImprovisedUser())
             ->withHeaders([
                 'Authorization' => 'Bearer '.$user->generateApiToken(),
             ])
             ->putJson(route('api.articles.massUpdate'), [
-                'articles' => $articles->modelKeys()
+                'articles' => [1,2,3]
             ])
             ->assertStatus(JsonResponse::HTTP_FORBIDDEN);
     }
@@ -279,7 +332,7 @@ class ArticlesControllerTest extends TestCase
      * @covers ::massUpdate
      *
      * Собственник сайта мог бы массово отредактировать записи,
-     * но он не передал никаких данных по ним.
+     * но он не передал некоторые данные по ним.
      * @return void
      */
     public function testOwnerCanNotMassUpdateArticleWithoutDataProvided(): void
@@ -334,12 +387,13 @@ class ArticlesControllerTest extends TestCase
                         'content',
                     ]
                 ]
-            ]);;
+            ]);
     }
 
     /**
      * Создать импровизированного пользователя.
      * @param  string  $role
+     * @param  array  $attributes
      * @return User
      */
     protected function createImprovisedUser(string $role = 'user', array $attributes = []): User
@@ -350,20 +404,12 @@ class ArticlesControllerTest extends TestCase
     }
 
     /**
-     * Выполнять дальнейшие действия от имени зарегистрированного пользователя.
-     * @param  UserContract  $user
-     * @param  string|null  $driver
+     * Выполнять дальнейшие действия
+     * от имени собственника сайта
+     * с передачей заголовка аутентификации.
      * @return self
-     *
-     * @NB Перепишем родительский метод, так как
-     * тестируем только api-драйвер аутентификации.
      */
-    public function actingAs(UserContract $user, $driver = null): self
-    {
-        return parent::actingAs($user, 'api');
-    }
-
-    protected function actingAsOwner()
+    protected function actingAsOwner(): self
     {
         return $this->actingAs($owner = $this->createImprovisedUser('owner'))
             ->withHeaders([
