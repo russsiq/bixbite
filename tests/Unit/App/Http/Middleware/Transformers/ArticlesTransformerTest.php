f513a978f5b5b845b8043df2cb36e0c43f9782a8
Рустам <rusiq@list.ru>
2020-06-08 11:28:03 +1000

    Create ArticlesTransformerTest.php
    php artisan make:test App\Http\Middleware\Transformers\ArticlesTransformerTest --unit

---
+++ tests/Unit/App/Http/Middleware/Transformers/ArticlesTransformerTest.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Tests\Unit\App\Http\Middleware\Transformers;
+
+use PHPUnit\Framework\TestCase;
+
+class ArticlesTransformerTest extends TestCase
+{
+    /**
+     * A basic unit test example.
+     *
+     * @return void
+     */
+    public function testExample()
+    {
+        $this->assertTrue(true);
+    }
+}
