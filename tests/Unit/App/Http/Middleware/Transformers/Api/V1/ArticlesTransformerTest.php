c7b6e73db6d73b9c335fc0d7c5b3719bf0fede7c
Рустам <rusiq@list.ru>
2020-06-08 23:04:24 +1000

    Update ArticlesTransformerTest.php
    Удален пробный тест

--- tests/Unit/App/Http/Middleware/Transformers/Api/V1/ArticlesTransformerTest.php
+++ tests/Unit/App/Http/Middleware/Transformers/Api/V1/ArticlesTransformerTest.php
@@ -8,6 +8,7 @@ use App\Http\Middleware\Transformers\Api\V1\ArticlesTransformer;
 // Сторонние зависимости.
 use App\Support\Contracts\ResourceRequestTransformer;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 // Библиотеки тестирования.
 use PHPUnit\Framework\TestCase;
@@ -19,41 +20,6 @@ use PHPUnit\Framework\TestCase;
  */
 class ArticlesTransformerTest extends TestCase
 {
-    /**
-     * @test
-     * @dataProvider additionProvider
-     *
-     * [testExample description]
-     * @param string $action
-     * @param array  $requestingInputs
-     * @param mixed  $expected
-     */
-    public function testExample(string $action, array $requestingInputs, $expected): void
-    {
-        $request = $this->createRequestWithCustomData($requestingInputs);
-        $transformer = $this->createTransformer($request);
-
-        $this->assertEquals($expected, $transformer->{$action}());
-    }
-
-    /**
-     * [additionProvider description]
-     * @return array
-     */
-    public function additionProvider(): array
-    {
-        return [
-            'верхний регистр заголовка' => [
-                'update', [
-                    'title' => 'Article title',
-                ], [
-                    'title' => 'ARTICLE TITLE',
-                ]
-            ],
-
-        ];
-    }
-
     protected function createTransformer(Request $request)
     {
         return new ArticlesTransformer($request);
