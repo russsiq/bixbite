6bdeb75f0c3d4588fb3f5fe2a982460d06dbf81b
Рустам <rusiq@list.ru>
2020-06-08 19:56:12 +1000

    Create UnsupportedMiddlewareForRouteException.php
    Нечего тут мокать

---
+++ app/Exceptions/UnsupportedMiddlewareForRouteException.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Exceptions;
+
+// Исключения.
+use RuntimeException;
+
+/**
+ * Исключение, выбрасываемое,
+ * когда применяемый посредник
+ * не предназначен для выбранного маршрута
+ */
+class UnsupportedMiddlewareForRouteException extends RuntimeException
+{
+    /**
+     * Создать новый экземпляр исключения.
+     * @param  object  $middleware
+     * @param  string  $route
+     * @return self
+     */
+    public static function make(string $middleware, string $route): self
+    {
+        return new self(sprintf(
+            'This middleware [%s] is not supported by this route [%s].',
+            $middleware,
+            $route
+        ));
+    }
+}
