f782ab87765c0abf5266e6fbc74d3e08e1e2a143
Рустам <rusiq@list.ru>
2020-06-08 12:28:08 +1000

    Update ArticlesTransformerTest.php
    Наполнение заглушками методов, описание зависимостей

--- tests/Unit/App/Http/Middleware/Transformers/Api/V1/ArticlesTransformerTest.php
+++ tests/Unit/App/Http/Middleware/Transformers/Api/V1/ArticlesTransformerTest.php
@@ -2,17 +2,74 @@
 
 namespace Tests\Unit\App\Http\Middleware\Transformers\Api\V1;
 
+// Тестируемый класс.
+use App\Http\Middleware\Transformers\Api\V1\ArticlesTransformer;
+
+// Сторонние зависимости.
+use App\Support\Contracts\ResourceRequestTransformer;
+use Illuminate\Http\Request;
+
+// Библиотеки тестирования.
 use PHPUnit\Framework\TestCase;
 
+/**
+ * @coversDefaultClass \App\Http\Middleware\Transformers\Api\V1\ArticlesTransformer
+ *
+ * @cmd phpunit tests\Unit\App\Http\Middleware\Transformers\Api\V1\ArticlesTransformerTest.php
+ */
 class ArticlesTransformerTest extends TestCase
 {
     /**
-     * A basic unit test example.
+     * @test
+     * @dataProvider additionProvider
      *
-     * @return void
+     * [testExample description]
+     * @param string $action
+     * @param array  $requestingInputs
+     * @param mixed  $expected
      */
-    public function testExample()
+    public function testExample(string $action, array $requestingInputs, $expected): void
     {
-        $this->assertTrue(true);
+        $request = $this->createRequestWithCustomData($requestingInputs);
+        $transformer = $this->createTransformer($request);
+
+        $this->assertEquals($expected, $transformer->{$action}());
+    }
+
+    /**
+     * [additionProvider description]
+     * @return array
+     */
+    public function additionProvider(): array
+    {
+        return [
+            'верхний регистр заголовка' => [
+                'update', [
+                    'title' => 'Article title',
+                ], [
+                    'title' => 'ARTICLE TITLE',
+                ]
+            ],
+
+        ];
+    }
+
+    protected function createTransformer(Request $request)
+    {
+        return new ArticlesTransformer($request);
+    }
+
+    /**
+     * Создать запрос с набором пользовательских данных.
+     * @param  array  $requestingInputs
+     * @return Request
+     */
+    protected function createRequestWithCustomData(array $requestingInputs): Request
+    {
+        $request = new Request;
+
+        $request->merge($requestingInputs);
+
+        return $request;
     }
 }
