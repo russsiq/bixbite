a8011ef18b57d9b6b3b3bc1421fab60297976ade
Рустам <rusiq@list.ru>
2020-06-09 21:51:24 +1000

    Реорганизация кода в api-маршрутах

--- routes/api.php
+++ routes/api.php
@@ -19,30 +19,36 @@ Route::group([
 
         ],
         'as' => 'api.'
+
     ], function () {
         Route::post('auth/logout', 'AuthController@logout')->name('auth.logout');
         Route::get('notes/form', 'NotesController@form')->name('notes.form');
         Route::get('settings/{module}', 'SettingsController@getModule')->name('settings.getModule');
         Route::put('settings/{module}', 'SettingsController@updateModule')->name('settings.updateModule');
 
-        Route::apiResources([
-            'articles' => 'ArticlesController',
-            'categories' => 'CategoriesController',
-            'comments' => 'CommentsController',
-            'files' => 'FilesController',
-            'notes' => 'NotesController',
-            'privileges' => 'PrivilegesController',
-            'settings' => 'SettingsController',
-            // 'tags' => 'TagsController',
-            'templates' => 'TemplatesController',
-            'users' => 'UsersController',
-            'x_fields' => 'XFieldsController',
+        Route::group([
+            'middleware' => [
+                TransformApiData::class,
 
-        ], [
-            'middleware' => TransformApiData::class,
+            ],
 
-        ]);
+        ], function () {
+            Route::apiResources([
+                'articles' => 'ArticlesController',
+                'categories' => 'CategoriesController',
+                'comments' => 'CommentsController',
+                'files' => 'FilesController',
+                'notes' => 'NotesController',
+                'privileges' => 'PrivilegesController',
+                'settings' => 'SettingsController',
+                // 'tags' => 'TagsController',
+                'templates' => 'TemplatesController',
+                'users' => 'UsersController',
+                'x_fields' => 'XFieldsController',
 
-        Route::put('articles', 'ArticlesController@massUpdate')->name('articles.massUpdate');
-        Route::put('comments', 'CommentsController@massUpdate')->name('comments.massUpdate');
+            ]);
+
+            Route::put('articles', 'ArticlesController@massUpdate')->name('articles.massUpdate');
+            Route::put('comments', 'CommentsController@massUpdate')->name('comments.massUpdate');
+        });
     });
