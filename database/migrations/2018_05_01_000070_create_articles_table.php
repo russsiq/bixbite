4a73709e26b89339dc822a20b7cbd0b0d608526b
Рустам <rusiq@list.ru>
2020-06-26 07:16:11 +1000

    Комментирование

--- database/migrations/2018_05_01_000070_create_articles_table.php
+++ database/migrations/2018_05_01_000070_create_articles_table.php
@@ -18,37 +18,37 @@ class CreateArticlesTable extends Migration
         Schema::create('articles', function (Blueprint $table) {
             $table->id();
 
-            // Relation and other indexed keys.
+            // Отношения и другие поля с индексами.
             $table->string('img')->nullable();
             $table->unsignedBigInteger('user_id');
             $table->unsignedBigInteger('image_id')->nullable();
-
-            // Main content.
             $table->enum('state', ['draft', 'unpublished', 'published'])->default('unpublished');
+
+            // Основное содержимое.
             $table->string('title');
             $table->string('slug');
             $table->string('teaser')->nullable();
-            $table->text('content');
+            $table->text('content')->nullable();
 
-            // Meta.
+            // Мета поля.
             $table->string('description')->nullable();
             $table->string('keywords')->nullable();
             $table->enum('robots', ['noindex', 'nofollow', 'none'])->nullable(); // null - content="all"
 
-            // Aditional.
+            // Дополнительная информация.
             $table->boolean('on_mainpage')->nullable()->default(1);
             $table->boolean('is_favorite')->nullable()->default(0);
             $table->boolean('is_pinned')->nullable()->default(0);
             $table->boolean('is_catpinned')->nullable()->default(0);
             $table->tinyInteger('allow_com')->unsigned()->default(2); // 0 - no; 1 - yes; 2 - by default
 
-            // Counters.
-            $table->unsignedInteger('shares')->default(0);
+            // Счетчики.
             $table->unsignedInteger('views')->default(0);
+            $table->unsignedInteger('shares')->default(0);
             $table->unsignedInteger('votes')->nullable();
             $table->unsignedInteger('rating')->nullable();
 
-            // Timestamps.
+            // Временные метки.
             $table->timestamps();
 
             // Make indexes.
