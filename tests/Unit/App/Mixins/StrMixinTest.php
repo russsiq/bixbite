85c880492281175efb6624a26ae1b070e44bf1b0
Рустам <rusiq@list.ru>
2020-07-10 15:41:22 +1000

    [Тесты] Заглушка для тестов миксина `Str`

---
+++ tests/Unit/App/Mixins/StrMixinTest.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace Tests\Unit\App\Mixins;
+
+// Тестируемый класс.
+use App\Mixins\StrMixin;
+
+// Сторонние зависимости.
+use Illuminate\Support\Str;
+
+// Библиотеки тестирования.
+use PHPUnit\Framework\TestCase;
+
+/**
+ * @coversDefaultClass \App\Mixins\StrMixin
+ *
+ * @cmd phpunit tests\Unit\App\Mixins\StrMixinTest.php
+ */
+class StrMixinTest extends TestCase
+{
+    protected function setUp(): void
+    {
+
+    }
+
+    /**
+     * A basic unit test example.
+     *
+     * @return void
+     */
+    public function testExample()
+    {
+        $this->assertTrue(true);
+    }
+}
