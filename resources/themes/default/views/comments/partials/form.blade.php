4de80b97264942630f06c0470914e6dbc77aa29e
Рустам <rusiq@list.ru>
2020-07-15 02:52:19 +1000

    Капча всегда должна присутствовать в форме отправки комментария

--- resources/themes/default/views/comments/partials/form.blade.php
+++ resources/themes/default/views/comments/partials/form.blade.php
@@ -9,8 +9,12 @@
             <div class="alert alert-info">@lang('comments.msg.regonly')</div>
         @else
             <form id="respond_form" action="{{ $entity->comment_store_action }}#respond" method="post">
-                @guest
-                    <div class="row">
+                <input type="hidden" name="_token" value="{{ pageinfo('csrf_token') }}" />
+                <input type="hidden" name="parent_id" value="{{ old('parent_id') }}" />
+
+
+                <div class="row">
+                    @guest
                         <div class="col-md-4 ">
                             <div class="form-group">
                                 <input type="text" name="name" value="{{ old('name') }}" class="form-control" placeholder="@lang('auth.name')" required />
@@ -28,17 +32,19 @@
                                 @endif
                             </div>
                         </div>
+                    @endguest
 
-                        <div class="col-md-4">
-                            {{-- @setting('system.captcha_used')
-                                @captcha('components.partials.captcha')
-                            @endsetting --}}
-                            @if (config('g_recaptcha.used'))
-                                @g_recaptcha_input
-                            @endif
+                    <div class="col-md-4">
+                        <div class="form-group">
+                        {{-- @setting('system.captcha_used')
+                            @captcha('components.partials.captcha')
+                        @endsetting --}}
+                        @if (config('g_recaptcha.used'))
+                            @g_recaptcha_input
+                        @endif
                         </div>
                     </div>
-                @endguest
+                </div>
 
                 <div class="form-group">
                     <textarea name="content" rows="8" class="form-control" placeholder="@lang('comments.content')" required>{{ old('content') }}</textarea>
@@ -50,9 +56,6 @@
                 <div class="form-group">@lang('comments.msg.rules')</div>
 
                 <div class="form-group-last">
-                    <input type="hidden" name="_token" value="{{ pageinfo('csrf_token') }}">
-                    <input type="hidden" name="parent_id" value="{{ old('parent_id') }}" />
-
                     <button type="submit" class="btn btn-outline-dark">@lang('comments.btn.post')</button>
 
                     {{-- Атрибуты `id` и `data-reply` используются JavaScript. --}}
