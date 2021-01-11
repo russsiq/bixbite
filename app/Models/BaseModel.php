a12c5654c9c05388ecc5c153ff54177ea264c562
Рустам <rusiq@list.ru>
2021-01-12 00:29:54 +1000

    Методы для получения примитивных типизаторов в Моделях

--- app/Models/BaseModel.php
+++ app/Models/BaseModel.php
@@ -6,5 +6,24 @@ use Illuminate\Database\Eloquent\Model;
 
 abstract class BaseModel extends Model
 {
+    /**
+     * Получить список встроенных примитивных типизаторов.
+     *
+     * @return array
+     */
+    public function primitiveCastTypes(): array
+    {
+        return static::$primitiveCastTypes;
+    }
 
+    /**
+     * Определить, что переданный тип относится к примитивной типизации.
+     *
+     * @param  string $castType
+     * @return bool
+     */
+    public function isPrimitiveCastTypes(string $castType): bool
+    {
+        return in_array($castType, primitiveCastTypes());
+    }
 }
