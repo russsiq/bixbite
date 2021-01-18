c2035a58ab43a6dc3a7c370e1e68960bf2be6f91
russsiq <rusiq@list.ru>
2021-01-18 18:58:09 +1000

    Разрешит создание пользователей только с ролью обычного `user`

--- app/Actions/Fortify/CreateNewUser.php
+++ app/Actions/Fortify/CreateNewUser.php
@@ -36,6 +36,8 @@ class CreateNewUser implements CreatesNewUsers
             'name' => $input['name'],
             'email' => $input['email'],
             'password' => Hash::make($input['password']),
+            // Only 'user'.
+            'role' => 'user',
         ]);
     }
 }
