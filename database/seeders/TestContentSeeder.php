a7c840c23a5a52df6fc07eb06a2113f85c9bfb4e
russsiq <rusiq@list.ru>
2021-01-25 12:32:48 +1000

    Добавит заготовку для наполнителя тестовыми данными Установщика

---
+++ database/seeders/TestContentSeeder.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+
+class TestContentSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        //
+    }
+}
