42b2c2a7b325b2eab07c99907d3cb679825f979f
russsiq <rusiq@list.ru>
2021-01-25 16:19:28 +1000

    Изменит шаблон для финальной стадии Установщика

--- resources/views/vendor/assistant/install/common.blade.php
+++ resources/views/vendor/assistant/install/common.blade.php
@@ -11,21 +11,61 @@
 	            @endforeach
 	        </ul>
 	    </div>
-	@endif
+    @endif
 
-	<fieldset>
-		<legend>@lang('assistant::install.forms.legends.enviroments')</legend>
+    <fieldset>
+        <legend>@lang('assistant::install.forms.legends.enviroments')</legend>
 		<div class="form-group row @error('APP_ENV') is-invalid @enderror">
-			<label class="col-sm-3 col-form-label">APP_ENV</label>
-			<div class="col-sm-9">
-				<select class="form-control" name="APP_ENV">
+            <label class="col-sm-3 col-form-label">APP_ENV</label>
+            <div class="col-sm-9">
+                <select class="form-control" name="APP_ENV">
 					@foreach (['production', 'local', 'dev'] as $env)
 						<option value="{{ $env }}">{{ $env }}</option>
 					@endforeach
 				</select>
-				@error ('APP_ENV')
+                @error ('APP_ENV')
+                    <div class="invalid-feedback d-block">{{ $message }}</div>
+                @enderror
+            </div>
+        </div>
+    </fieldset>
+
+	<fieldset>
+		<legend>@lang('Register')</legend>
+		<div class="mb-3 row @error('name') is-invalid @enderror">
+			<label class="col-sm-3 col-form-label">@lang('Name')</label>
+			<div class="col-sm-9">
+				<input type="text" name="name" value="{{ old('name', '') }}" placeholder="admin" class="form-control" />
+				@error ('name')
 					<div class="invalid-feedback d-block">{{ $message }}</div>
 				@enderror
+			</div>
+		</div>
+
+		<div class="mb-3 row @error('email') is-invalid @enderror">
+			<label class="col-sm-3 col-form-label">@lang('Email')</label>
+			<div class="col-sm-9">
+				<input type="email" name="email" value="{{ old('email', '') }}" placeholder="{{ $email }}" class="form-control" />
+				@error ('email')
+					<div class="invalid-feedback d-block">{{ $message }}</div>
+				@enderror
+			</div>
+		</div>
+
+		<div class="mb-3 row @error('password') is-invalid @enderror">
+			<label class="col-sm-3 col-form-label">@lang('Password')</label>
+			<div class="col-sm-9">
+				<input type="password" name="password" value="" placeholder="********" class="form-control" autocomplete="new-password"  />
+				@error ('password')
+					<div class="invalid-feedback d-block">{{ $message }}</div>
+				@enderror
+			</div>
+		</div>
+
+		<div class="mb-3 row">
+			<label class="col-sm-3 col-form-label">@lang('Confirm Password')</label>
+			<div class="col-sm-9">
+				<input type="password" name="password_confirmation" placeholder="********" class="form-control" autocomplete="new-password"  />
 			</div>
 		</div>
 	</fieldset>
