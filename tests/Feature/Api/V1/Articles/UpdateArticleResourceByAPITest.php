720e0d980bba61fdd997ae139f7cf0da2de1c123
russsiq <rusiq@list.ru>
2021-02-01 23:52:11 +1000

    Добавит `test_user_can_update_article`

--- tests/Feature/Api/V1/Articles/UpdateArticleResourceByAPITest.php
+++ tests/Feature/Api/V1/Articles/UpdateArticleResourceByAPITest.php
@@ -5,8 +5,10 @@ declare(strict_types=1);
 namespace Tests\Feature\Api\V1\Articles;
 
 use App\Models\Article;
+use App\Models\User;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
+use Laravel\Sanctum\Sanctum;
 use Tests\TestCase;
 
 /**
@@ -27,5 +29,20 @@ class UpdateArticleResourceByAPITest extends TestCase
 
             ])
             ->assertUnauthorized();
+    }
+
+    public function test_user_can_update_article()
+    {
+        Sanctum::actingAs(
+            $user = User::factory()->create()
+        );
+
+        $article = Article::factory()->create();
+
+        $response = $this->assertAuthenticated()
+           ->putJson(route('api.articles.update', $article), [
+
+            ])
+            ->assertStatus(202);
     }
 }
