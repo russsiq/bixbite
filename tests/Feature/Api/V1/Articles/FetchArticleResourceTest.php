8a762a6d1e45b060f0981c28c43910126a0cd358
russsiq <rusiq@list.ru>
2021-02-01 14:21:27 +1000

    Добавит `test_user_can_fetch_articles`

--- tests/Feature/Api/V1/Articles/FetchArticleResourceTest.php
+++ tests/Feature/Api/V1/Articles/FetchArticleResourceTest.php
@@ -4,6 +4,8 @@ declare(strict_types=1);
 
 namespace Tests\Feature\Api\V1\Articles;
 
+use App\Models\User;
+use Laravel\Sanctum\Sanctum;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
@@ -15,10 +17,23 @@ use Tests\TestCase;
  */
 class FetchArticleResourceTest extends TestCase
 {
+    use RefreshDatabase;
+
     public function test_guest_cannot_fetch_articles()
     {
         $response = $this->assertGuest()
             ->getJson(route('api.articles.index'))
             ->assertUnauthorized();
+    }
+
+    public function test_user_can_fetch_articles()
+    {
+        Sanctum::actingAs(
+            $user = User::factory()->create()
+        );
+
+        $response = $this->assertAuthenticated()
+            ->getJson(route('api.articles.index'))
+            ->assertOk();
     }
 }
