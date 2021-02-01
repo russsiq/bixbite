5148b1510fd08d4b546eca0d627525043109871b
russsiq <rusiq@list.ru>
2021-02-01 13:56:54 +1000

    Вернет обработку исключений для `test_guest_cannot_fetch_articles`

--- tests/Feature/Api/V1/Articles/FetchArticleResourceTest.php
+++ tests/Feature/Api/V1/Articles/FetchArticleResourceTest.php
@@ -17,8 +17,6 @@ class FetchArticleResourceTest extends TestCase
 {
     public function test_guest_cannot_fetch_articles()
     {
-        $this->withoutExceptionHandling();
-
         $response = $this->assertGuest()
             ->getJson('api/v1/articles')
             ->assertUnauthorized();
