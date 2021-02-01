2b182fe8a06be12cf921773fbb0ea539e2c33134
russsiq <rusiq@list.ru>
2021-02-01 23:58:51 +1000

    Добавит `test_guest_cannot_delete_article`

--- tests/Feature/Api/V1/Articles/DeleteArticleResourceByAPITest.php
+++ tests/Feature/Api/V1/Articles/DeleteArticleResourceByAPITest.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 namespace Tests\Feature\Api\V1\Articles;
 
+use App\Models\Article;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
@@ -16,4 +17,15 @@ use Tests\TestCase;
 class DeleteArticleResourceByAPITest extends TestCase
 {
     use RefreshDatabase;
+
+    public function test_guest_cannot_delete_article()
+    {
+        $article = Article::factory()->create();
+
+        $response = $this->assertGuest()
+            ->deleteJson(route('api.articles.destroy', $article), [
+
+            ])
+            ->assertUnauthorized();
+    }
 }
