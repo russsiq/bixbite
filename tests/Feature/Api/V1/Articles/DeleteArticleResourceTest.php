668ec6f753d287005e007c67cfc7e648ff75b9b6
russsiq <rusiq@list.ru>
2021-02-01 19:50:11 +1000

    Удалит примеры тестов для ресурса Article

--- tests/Feature/Api/V1/Articles/DeleteArticleResourceTest.php
+++ tests/Feature/Api/V1/Articles/DeleteArticleResourceTest.php
@@ -8,15 +8,4 @@ use Tests\TestCase;
 
 class DeleteArticleResourceTest extends TestCase
 {
-    /**
-     * A basic feature test example.
-     *
-     * @return void
-     */
-    public function test_example()
-    {
-        $response = $this->get('/');
-
-        $response->assertStatus(200);
-    }
 }
--- tests/Feature/Api/V1/Articles/UpdateArticleResourceTest.php
+++ tests/Feature/Api/V1/Articles/UpdateArticleResourceTest.php
@@ -8,15 +8,4 @@ use Tests\TestCase;
 
 class UpdateArticleResourceTest extends TestCase
 {
-    /**
-     * A basic feature test example.
-     *
-     * @return void
-     */
-    public function test_example()
-    {
-        $response = $this->get('/');
-
-        $response->assertStatus(200);
-    }
 }
