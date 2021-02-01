ad063e7224e0b0d7402445959a459fc75aacc82a
russsiq <rusiq@list.ru>
2021-02-01 23:41:41 +1000

    Добавит `test_guest_cannot_update_article`

--- tests/Feature/Api/V1/Articles/UpdateArticleResourceByAPITest.php
+++ tests/Feature/Api/V1/Articles/UpdateArticleResourceByAPITest.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 namespace Tests\Feature\Api\V1\Articles;
 
+use App\Models\Article;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
@@ -16,4 +17,15 @@ use Tests\TestCase;
 class UpdateArticleResourceByAPITest extends TestCase
 {
     use RefreshDatabase;
+
+    public function test_guest_cannot_update_article()
+    {
+        $article = Article::factory()->create();
+
+        $response = $this->assertGuest()
+            ->putJson(route('api.articles.update', $article), [
+
+            ])
+            ->assertUnauthorized();
+    }
 }
