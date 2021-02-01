6678d697f4e0eb0d0049007234524550123744e7
russsiq <rusiq@list.ru>
2021-02-01 20:18:59 +1000

    Добавит `test_user_can_create_article`

--- tests/Feature/Api/V1/Articles/CreateArticleResourceByAPITest.php
+++ tests/Feature/Api/V1/Articles/CreateArticleResourceByAPITest.php
@@ -4,6 +4,8 @@ declare(strict_types=1);
 
 namespace Tests\Feature\Api\V1\Articles;
 
+use App\Models\User;
+use Laravel\Sanctum\Sanctum;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
@@ -24,5 +26,18 @@ class CreateArticleResourceByAPITest extends TestCase
 
             ])
             ->assertUnauthorized();
+    }
+
+    public function test_user_can_create_article()
+    {
+        Sanctum::actingAs(
+            $user = User::factory()->create()
+        );
+
+        $response = $this->assertAuthenticated()
+            ->postJson(route('api.articles.store'), [
+
+            ])
+            ->assertCreated();
     }
 }
