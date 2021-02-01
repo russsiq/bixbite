bd2a64fec29a367638ea9b3ee17700aa8281d745
russsiq <rusiq@list.ru>
2021-02-02 00:01:04 +1000

    Добавит `test_user_can_delete_article`

--- tests/Feature/Api/V1/Articles/DeleteArticleResourceByAPITest.php
+++ tests/Feature/Api/V1/Articles/DeleteArticleResourceByAPITest.php
@@ -5,8 +5,10 @@ declare(strict_types=1);
 namespace Tests\Feature\Api\V1\Articles;
 
 use App\Models\Article;
+use App\Models\User;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
+use Laravel\Sanctum\Sanctum;
 use Tests\TestCase;
 
 /**
@@ -27,5 +29,20 @@ class DeleteArticleResourceByAPITest extends TestCase
 
             ])
             ->assertUnauthorized();
+    }
+
+    public function test_user_can_delete_article()
+    {
+        Sanctum::actingAs(
+            $user = User::factory()->create()
+        );
+
+        $article = Article::factory()->create();
+
+        $response = $this->assertAuthenticated()
+           ->deleteJson(route('api.articles.destroy', $article), [
+
+            ])
+            ->assertNoContent();
     }
 }
