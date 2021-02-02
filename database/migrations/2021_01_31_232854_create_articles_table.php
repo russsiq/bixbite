336e17423df0f5448e91051e6b557830896df593
russsiq <rusiq@list.ru>
2021-02-03 01:29:52 +1000

    Добавит мин.-необходимый набор полей таблицы `articles`

--- database/migrations/2021_01_31_232854_create_articles_table.php
+++ database/migrations/2021_01_31_232854_create_articles_table.php
@@ -15,7 +15,36 @@ class CreateArticlesTable extends Migration
     {
         Schema::create('articles', function (Blueprint $table) {
             $table->id();
+
+            // Отношения и индексные поля.
+            $table->foreignId('user_id')
+                ->constrained('users')
+                ->onDelete('cascade');
+
+            // Основное содержимое.
+            $table->string('title');
+            $table->string('slug')->unique();
+            $table->text('teaser')->nullable();
+            $table->text('content')->nullable();
+
+            // SEO-поля для метатегов.
+            $table->string('meta_description')->nullable();
+            $table->string('meta_keywords')->nullable();
+            $table->enum('meta_robots', ['all', 'noindex', 'nofollow', 'none'])
+                ->default('all');
+
+            // Поля с дополнительной информацией.
+            $table->boolean('on_mainpage')->default(1);
+            $table->boolean('is_favorite')->default(0);
+            $table->boolean('is_pinned')->default(0);
+
+            // Счетчики.
+            $table->unsignedInteger('views')->default(0);
+
+            // Временные метки.
             $table->timestamps();
+
+            // Создание индексов.
         });
     }
 
