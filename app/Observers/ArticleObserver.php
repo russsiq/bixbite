687ad27c99587c6785a1a63ce8d178e447e6893b
russsiq <rusiq@list.ru>
2021-02-02 19:29:27 +1000

    Создаст и зарегистрирует наблюдателей для моделей Article, User

---
+++ app/Observers/ArticleObserver.php
@@ -0,0 +1,64 @@
+<?php
+
+namespace App\Observers;
+
+use App\Models\Article;
+use Illuminate\Support\Facades\Auth;
+
+class ArticleObserver
+{
+    /**
+     * Handle the Article "created" event.
+     *
+     * @param  \App\Models\Article  $article
+     * @return void
+     */
+    public function created(Article $article)
+    {
+        //
+    }
+
+    /**
+     * Handle the Article "updated" event.
+     *
+     * @param  \App\Models\Article  $article
+     * @return void
+     */
+    public function updated(Article $article)
+    {
+        //
+    }
+
+    /**
+     * Handle the Article "deleted" event.
+     *
+     * @param  \App\Models\Article  $article
+     * @return void
+     */
+    public function deleted(Article $article)
+    {
+        //
+    }
+
+    /**
+     * Handle the Article "restored" event.
+     *
+     * @param  \App\Models\Article  $article
+     * @return void
+     */
+    public function restored(Article $article)
+    {
+        //
+    }
+
+    /**
+     * Handle the Article "force deleted" event.
+     *
+     * @param  \App\Models\Article  $article
+     * @return void
+     */
+    public function forceDeleted(Article $article)
+    {
+        //
+    }
+}
--- app/Providers/EventServiceProvider.php
+++ app/Providers/EventServiceProvider.php
@@ -2,6 +2,10 @@
 
 namespace App\Providers;
 
+use App\Models\User;
+use App\Models\Article;
+use App\Observers\UserObserver;
+use App\Observers\ArticleObserver;
 use Illuminate\Auth\Events\Registered;
 use Illuminate\Auth\Listeners\SendEmailVerificationNotification;
 use Illuminate\Foundation\Support\Providers\EventServiceProvider as ServiceProvider;
@@ -27,6 +31,7 @@ class EventServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
+        User::observe(UserObserver::class);
+        Article::observe(ArticleObserver::class);
     }
 }
