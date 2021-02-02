6459844db9e4ef9c73c7114fc383e74c0e23e31b
russsiq <rusiq@list.ru>
2021-02-03 01:50:55 +1000

    Добавит типизацию и массово-заполняемые атрибуты модели Article

--- app/Models/Article.php
+++ app/Models/Article.php
@@ -2,10 +2,48 @@
 
 namespace App\Models;
 
+use App\Models\User;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Article extends Model
 {
     use HasFactory;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var string[]
+     */
+    protected $fillable = [
+        // 'user_id',
+        'title', 'slug', 'teaser', 'content',
+        'meta_description', 'meta_keywords', 'meta_robots',
+        'on_mainpage', 'is_favorite', 'is_pinned',
+    ];
+
+    /**
+     * The attributes that should be cast.
+     *
+     * @var array
+     */
+    protected $casts = [
+        'user_id' => 'integer',
+
+        'title' => 'string',
+        'slug' => 'string',
+        'teaser' => 'string',
+        'content' => 'string',
+
+        'meta_description' => 'string',
+        'meta_keywords' => 'string',
+        'meta_robots' => 'string',
+
+        'on_mainpage' => 'boolean',
+        'is_favorite' => 'boolean',
+        'is_pinned' => 'boolean',
+
+        'views' => 'integer',
+    ];
 }
