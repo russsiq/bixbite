6f508544578fa8bcbe6d37575134d0e81f2e4479
russsiq <rusiq@list.ru>
2021-02-03 01:52:48 +1000

    Добавит валидацию запроса из формы создания Article

--- app/Http/Requests/Api/V1/Article/StoreArticleRequest.php
+++ app/Http/Requests/Api/V1/Article/StoreArticleRequest.php
@@ -3,6 +3,7 @@
 namespace App\Http\Requests\Api\V1\Article;
 
 use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Validation\Rule;
 
 class StoreArticleRequest extends FormRequest
 {
@@ -14,7 +15,22 @@ class StoreArticleRequest extends FormRequest
     public function rules()
     {
         return [
+            // Не валидируем эти поля, оставляем управление
+            // наблюдателям, контроллерам и другой нечисти.
+            // 'user_id' => [],
 
+            'title' => ['required', 'string', 'max:255'],
+            'slug' => ['sometimes', 'string', 'max:255', 'alpha_dash', Rule::unique('articles')/*->ignore($this->id)*/,],
+            'teaser' => ['nullable', 'string', 'max:255'],
+            'content' => ['nullable', 'string', 'max:255'],
+
+            'meta_description' => ['nullable', 'string', 'max:255'],
+            'meta_keywords' => ['nullable', 'string', 'max:255'],
+            'meta_robots' => ['nullable', 'string', 'max:255', 'in:all,noindex,nofollow,none'],
+
+            'on_mainpage' => ['nullable', 'boolean'],
+            'is_favorite' => ['nullable', 'boolean'],
+            'is_pinned' => ['nullable', 'boolean'],
         ];
     }
 }
