33710cec893bfcf9a2824ceebcfb16e9906d81d7
russsiq <rusiq@list.ru>
2021-02-05 00:09:33 +1000

    Update Article.php

--- app/Models/Article.php
+++ app/Models/Article.php
@@ -49,6 +49,6 @@ class Article extends Model
 
     public function user(): BelongsTo
     {
-        return $this->belongsTo(User::class, 'user_id', 'id');
+        return $this->belongsTo(User::class, 'user_id', 'id', 'user');
     }
 }
