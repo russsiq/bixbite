76df5350995a76b38b3943bb632b1288750832f4
russsiq <rusiq@list.ru>
2021-02-06 20:02:01 +1000

    Update UserResource.php

--- app/Http/Resources/UserResource.php
+++ app/Http/Resources/UserResource.php
@@ -3,6 +3,7 @@
 namespace App\Http\Resources;
 
 use Illuminate\Http\Resources\Json\JsonResource;
+use Illuminate\Support\Arr;
 
 class UserResource extends JsonResource
 {
@@ -14,6 +15,29 @@ class UserResource extends JsonResource
      */
     public function toArray($request)
     {
-        return parent::toArray($request);
+        $attributes = parent::toArray($request);
+
+        return [
+            'type' => 'users',
+            'id' => $attributes['id'],
+            'attributes' => Arr::except($attributes, [
+                'id',
+            ]),
+        ];
+    }
+
+    /**
+     * Get any additional data that should be returned with the resource array.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return array
+     */
+    public function with($request)
+    {
+        return [
+            'links' => [
+                'self' => route('api.users.show', $this->resource),
+            ],
+        ];
     }
 }
