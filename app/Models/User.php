e3f6a97c0635acfb7afd20e35cf3c1f291b82c3c
russsiq <rusiq@list.ru>
2021-02-07 22:06:42 +1000

    Добавит в зависимость пакет `laravel/sanctum`
    Выполнены команды:
    
        composer require laravel/sanctum
        php artisan vendor:publish --provider="Laravel\Sanctum\SanctumServiceProvider"

--- app/Models/User.php
+++ app/Models/User.php
@@ -7,9 +7,11 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Fortify\TwoFactorAuthenticatable;
+use Laravel\Sanctum\HasApiTokens;
 
 class User extends Authenticatable implements MustVerifyEmail
 {
+    use HasApiTokens;
     use HasFactory;
     use Notifiable;
     use TwoFactorAuthenticatable;
---
+++ resources/views/panel.blade.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>Control Panel</title>
+</head>
+<body>
+    <h1>Control Panel</h1>
+</body>
+</html>
