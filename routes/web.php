f914f0231783664ff73ba19edfaa3b1c95d341cf
russsiq <rusiq@list.ru>
2021-02-08 08:26:20 +1000

    Добавит имя `home` маршруту главной страницы

--- routes/web.php
+++ routes/web.php
@@ -15,7 +15,7 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
-});
+})->name('home');
 
 Route::middleware(['auth:sanctum', 'verified'])->get('/panel', function () {
     return view('panel');
