53121db7883e1596bc325b73fca0fc821756e75c
russsiq <rusiq@list.ru>
2021-02-08 21:40:41 +1000

    Добавит ресурсный контроллер для Article

---
+++ app/Http/Controllers/Api/V1/ArticleController.php
@@ -0,0 +1,136 @@
+<?php
+
+namespace App\Http\Controllers\Api\V1;
+
+use App\Http\Controllers\Controller;
+use App\Http\Requests\Api\V1\Article\IndexArticleRequest;
+use App\Http\Requests\Api\V1\Article\StoreArticleRequest;
+use App\Http\Requests\Api\V1\Article\UpdateArticleRequest;
+use App\Http\Resources\ArticleCollection;
+use App\Http\Resources\ArticleResource;
+use App\Models\Article;
+use Illuminate\Http\JsonResponse;
+use Illuminate\Http\Request;
+
+class ArticleController extends Controller
+{
+    /**
+     * Create the controller instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        $this->authorizeResource(Article::class, 'article');
+    }
+
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index(IndexArticleRequest $request)
+    {
+        $articles = Article::with([
+            'user',
+        ])->paginate();
+
+        $collection = new ArticleCollection($articles);
+
+        return $collection->response()
+            ->setStatusCode(JsonResponse::HTTP_OK);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  StoreArticleRequest  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(StoreArticleRequest $request)
+    {
+        /** @var \App\Models\User $user */
+        $user = $request->user();
+
+        /** @var \App\Models\Article $articleInstance */
+        $article = $user->articles()
+            ->create($request->validated());
+
+        $resource = new ArticleResource(
+            $article->refresh()
+        );
+
+        return $resource->response()
+            ->setStatusCode(JsonResponse::HTTP_CREATED);
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Article  $article
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Article $article)
+    {
+        $resource = new ArticleResource($article);
+
+        return $resource->response()
+            ->setStatusCode(JsonResponse::HTTP_OK);
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\Article  $article
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Article $article)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  UpdateArticleRequest  $request
+     * @param  \App\Models\Article  $article
+     * @return \Illuminate\Http\Response
+     */
+    public function update(UpdateArticleRequest $request, Article $article)
+    {
+        $article->update(
+            $request->validated()
+        );
+
+        $resource = new ArticleResource(
+            $article->refresh()
+        );
+
+        return $resource->response()
+            ->setStatusCode(JsonResponse::HTTP_ACCEPTED);
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Article  $article
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Article $article)
+    {
+        $article->delete();
+
+        return response()
+            ->json(null, JsonResponse::HTTP_NO_CONTENT);
+    }
+}
