c1cdb0c0bb1bf6382a27e7fbfb10467992104346
russsiq <rusiq@list.ru>
2021-02-08 14:58:28 +1000

    Добавит маршрут и контроллер для профиля пользователя

--- routes/web.php
+++ routes/web.php
@@ -38,6 +38,11 @@ Route::middleware(['auth:sanctum', 'verified', 'password.confirm'])->get('/panel
     return view('panel');
 })->name('panel');
 
+Route::group(['middleware' => ['web', 'auth', 'verified']], function () {
+    // User & Profile...
+    Route::get('/user/profile', [UserProfileController::class, 'show'])
+        ->name('profile.show');
+});
 
 Route::group(['middleware' => config('fortify.middleware', ['web'])], function () {
     $enableViews = config('fortify.views', true);
