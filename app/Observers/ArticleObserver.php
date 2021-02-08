4f05653265dca77804ccd4bcc23cf942307f45cb
russsiq <rusiq@list.ru>
2021-02-08 21:40:14 +1000

    Добавит наблюдателей для моделей Article, User

---
+++ app/Observers/ArticleObserver.php
@@ -0,0 +1,75 @@
+<?php
+
+namespace App\Observers;
+
+use App\Models\Article;
+use Illuminate\Support\Str;
+
+class ArticleObserver
+{
+    /**
+     * Handle the Article "created" event.
+     *
+     * @param  \App\Models\Article  $article
+     * @return void
+     */
+    public function created(Article $article)
+    {
+        //
+    }
+
+    /**
+     * Handle the Article "updated" event.
+     *
+     * @param  \App\Models\Article  $article
+     * @return void
+     */
+    public function updated(Article $article)
+    {
+        //
+    }
+
+    /**
+     * Handle the Article "saving" event.
+     *
+     * @param  \App\Models\Article  $article
+     * @return void
+     */
+    public function saving(Article $article)
+    {
+        //
+    }
+
+    /**
+     * Handle the Article "deleted" event.
+     *
+     * @param  \App\Models\Article  $article
+     * @return void
+     */
+    public function deleted(Article $article)
+    {
+        //
+    }
+
+    /**
+     * Handle the Article "restored" event.
+     *
+     * @param  \App\Models\Article  $article
+     * @return void
+     */
+    public function restored(Article $article)
+    {
+        //
+    }
+
+    /**
+     * Handle the Article "force deleted" event.
+     *
+     * @param  \App\Models\Article  $article
+     * @return void
+     */
+    public function forceDeleted(Article $article)
+    {
+        //
+    }
+}
