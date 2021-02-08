f715fd6232305c60162c699fd866909cb190fc41
russsiq <rusiq@list.ru>
2021-02-08 21:37:50 +1000

    Добавит маршрут ресурса Article

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\Api\V1\ArticleController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -16,4 +17,19 @@ use Illuminate\Support\Facades\Route;
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
+});
+
+Route::group([
+    'as' => 'api.',
+    'prefix' => 'v1',
+], function () {
+    Route::group([
+        'middleware' => [
+            'auth:sanctum',
+        ],
+    ], function () {
+        Route::apiResources([
+            'articles' => ArticleController::class,
+        ]);
+    });
 });
