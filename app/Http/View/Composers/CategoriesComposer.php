a5176026778632a09ca07d044cea05fad8a4d351
russsiq <rusiq@list.ru>
2021-02-09 20:32:30 +1000

    Создаст компоновщика шаблона для коллекции категорий

---
+++ app/Http/View/Composers/CategoriesComposer.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace App\Http\View\Composers;
+
+use App\Models\Category;
+use App\Models\Collections\CategoryCollection;
+use Illuminate\View\View;
+
+class CategoriesComposer
+{
+    /** @var CategoryCollection */
+    protected static $categories;
+
+    /**
+     * Create a new profile composer.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+    }
+
+    /**
+     * Bind data to the view.
+     *
+     * @param  \Illuminate\View\View  $view
+     * @return void
+     */
+    public function compose(View $view)
+    {
+        $view->with('categories', $this->categories());
+    }
+
+    protected function categories(): CategoryCollection
+    {
+        return self::$categories
+            ?? self::$categories = $this->resolveCategories();
+    }
+
+    protected function resolveCategories(): CategoryCollection
+    {
+        return Category::query()
+            ->orderBy('position')
+            ->get()
+            ->nested();
+    }
+}
