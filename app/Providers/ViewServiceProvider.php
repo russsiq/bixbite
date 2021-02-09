a140100dcfc6f76cc0c3534dfc73fcbba960e50e
russsiq <rusiq@list.ru>
2021-02-09 20:25:29 +1000

    Создаст и зарегистрирует `ViewServiceProvider`

---
+++ app/Providers/ViewServiceProvider.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Providers;
+
+use Illuminate\Support\Facades\View;
+use Illuminate\Support\ServiceProvider;
+
+class ViewServiceProvider extends ServiceProvider
+{
+    /**
+     * Register any application services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        //
+    }
+
+    /**
+     * Bootstrap any application services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        //
+    }
+}
