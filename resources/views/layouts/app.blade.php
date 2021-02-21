f328f1cb0cb52dc84085fe9ddc7f864a971fb8cd
russsiq <rusiq@list.ru>
2021-02-22 01:16:54 +1000

    Update app.blade.php

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,44 +1,44 @@
 @extends('layouts.main')
 
 @push('styles')
-<link href="{{ mix('/css/app.css') }}" rel="stylesheet">
+    <link href="{{ mix('/css/app.css') }}" rel="stylesheet">
 @endpush
 
 @push('scripts')
-<script src="{{ mix('/js/app.js') }}" charset="utf-8"></script>
+    <script src="{{ mix('/js/app.js') }}" charset="utf-8"></script>
 @endpush
 
 @section('body')
-<x-navbar container="container" />
+    <x-navbar container="container" />
 
-@hasSection('header')
-<header class="my-4">
-    <div class="container">
-        <div class="row">
-            <div class="col">
-                @yield('header')
+    @hasSection('header')
+    <header class="my-4">
+        <div class="container">
+            <div class="row">
+                <div class="col">
+                    @yield('header')
+                </div>
             </div>
         </div>
-    </div>
-</header>
-@endif
+    </header>
+    @endif
 
-<main class="flex-shrink-0">
-    @yield('mainblock')
-</main>
+    <main class="flex-shrink-0">
+        @yield('mainblock')
+    </main>
 
-<footer class="footer mt-auto py-3 bg-light">
-    <div class="container">
-        <div class="row">
-            <div class="col">
-                <span class="text-muted">Laravel v{{ Illuminate\Foundation\Application::VERSION }}</span>
-            </div>
-            <div class="col text-end">
-                <span class="text-muted">(PHP v{{ PHP_VERSION }})</span>
+    <footer class="footer mt-auto py-3 bg-light">
+        <div class="container">
+            <div class="row">
+                <div class="col">
+                    <span class="text-muted">Laravel v{{ Illuminate\Foundation\Application::VERSION }}</span>
+                </div>
+                <div class="col text-end">
+                    <span class="text-muted">(PHP v{{ PHP_VERSION }})</span>
+                </div>
             </div>
         </div>
-    </div>
-</footer>
+    </footer>
 
-<x-consent-cookie />
+    <x-consent-cookie />
 @endsection
