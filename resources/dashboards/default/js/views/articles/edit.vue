27fc0fc948c0695bd784c2110b6539ca868e857e
russsiq <rusiq@list.ru>
2021-02-23 19:09:34 +1000

    Добавит заготовки шаблонов для редактирования `article` и `category`
    ... в Панели управления

---
+++ resources/dashboards/default/js/views/articles/edit.vue
@@ -0,0 +1,48 @@
+<template>
+    <div class="container mt-5">
+        <div v-if="article && article.id" class="card">
+            <h6 class="card-header">Edit article [{{ article.attributes.title }}]</h6>
+            <div class="card-body"></div>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "articles-edit",
+
+    props: {
+        model: {
+            type: Function,
+            required: true,
+        },
+
+        id: {
+            type: Number,
+            required: true,
+        },
+    },
+
+    data() {
+        return {
+            article: null,
+        };
+    },
+
+    mounted() {
+        this.$props.model
+            .$get({
+                params: {
+                    id: this.$props.id,
+                },
+            })
+            .then(this.fillForm);
+    },
+
+    methods: {
+        fillForm(article) {
+            this.article = article;
+        },
+    },
+};
+</script>
---
+++ resources/dashboards/default/js/views/categories/edit.vue
@@ -0,0 +1,48 @@
+<template>
+    <div class="container mt-5">
+        <div v-if="category && category.id" class="card">
+            <h6 class="card-header">Edit category [{{ category.attributes.title }}]</h6>
+            <div class="card-body"></div>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "categories-edit",
+
+    props: {
+        model: {
+            type: Function,
+            required: true,
+        },
+
+        id: {
+            type: Number,
+            required: true,
+        },
+    },
+
+    data() {
+        return {
+            category: null,
+        };
+    },
+
+    mounted() {
+        this.$props.model
+            .$get({
+                params: {
+                    id: this.$props.id,
+                },
+            })
+            .then(this.fillForm);
+    },
+
+    methods: {
+        fillTable(entity) {
+            this.category = entity;
+        },
+    },
+};
+</script>
