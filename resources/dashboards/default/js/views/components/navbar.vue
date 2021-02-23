54dfa5eddb5bd81cce1cbe193d6a747f145c8943
russsiq <rusiq@list.ru>
2021-02-24 02:08:11 +1000

    Выделит `navbar` в отдельный компонент в Панели управления

---
+++ resources/dashboards/default/js/views/components/navbar.vue
@@ -0,0 +1,53 @@
+<template>
+    <nav class="navbar navbar-expand-lg sticky-top navbar-dark bg-primary">
+        <div class="container-fluid">
+            <a class="navbar-brand" :href="appUrl">{{ appName }}</a>
+            <button
+                class="navbar-toggler"
+                type="button"
+                data-bs-toggle="collapse"
+                data-bs-target="#main-navbar"
+                aria-controls="main-navbar"
+                aria-expanded="false"
+                aria-label="Toggle navigation"
+            >
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div id="main-navbar" class="collapse navbar-collapse">
+                <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                    <li class="nav-item">
+                        <router-link to="/" class="nav-link" exact>Home</router-link>
+                    </li>
+                    <li class="nav-item">
+                        <router-link :to="{name: 'articles.index'}" class="nav-link">Articles</router-link>
+                    </li>
+                    <li class="nav-item">
+                        <router-link :to="{name: 'categories.index'}" class="nav-link">Categories</router-link>
+                    </li>
+                    <li class="nav-item">
+                        <router-link :to="{name: 'comments.index'}" class="nav-link">Comments</router-link>
+                    </li>
+                    <li class="nav-item">
+                        <router-link :to="{name: 'tags.index'}" class="nav-link">Tags</router-link>
+                    </li>
+                    <li class="nav-item">
+                        <router-link :to="{name: 'users.index'}" class="nav-link">Users</router-link>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </nav>
+</template>
+
+<script>
+export default {
+    name: "navbar",
+
+    data() {
+        return {
+            appUrl: this.$root.app_url,
+            appName: this.$root.app_name,
+        };
+    },
+};
+</script>
--- resources/dashboards/default/js/views/layouts/app.vue
+++ resources/dashboards/default/js/views/layouts/app.vue
@@ -1,34 +1,6 @@
 <template>
     <div class="app__body">
-        <nav class="navbar navbar-expand-lg sticky-top navbar-dark bg-primary">
-            <div class="container-fluid">
-                <a class="navbar-brand" :href="$root.app_url">{{ $root.app_name }}</a>
-                <button
-                    class="navbar-toggler"
-                    type="button"
-                    data-bs-toggle="collapse"
-                    data-bs-target="#main-navbar"
-                    aria-controls="main-navbar"
-                    aria-expanded="false"
-                    aria-label="Toggle navigation"
-                >
-                    <span class="navbar-toggler-icon"></span>
-                </button>
-                <div id="main-navbar" class="collapse navbar-collapse">
-                    <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-                        <li class="nav-item">
-                            <router-link to="/" class="nav-link" exact>Home</router-link>
-                        </li>
-                        <li class="nav-item">
-                            <router-link :to="{name: 'articles.index'}" class="nav-link">Articles</router-link>
-                        </li>
-                        <li class="nav-item">
-                            <router-link :to="{name: 'categories.index'}" class="nav-link">Categories</router-link>
-                        </li>
-                    </ul>
-                </div>
-            </div>
-        </nav>
+        <navbar />
         <main class="container">
             <router-view></router-view>
         </main>
@@ -39,7 +11,7 @@
 
 <script>
 import LoadingLayer from "@/views/components/loading-layer";
-import Toast from "@/helpers/notification";
+import Navbar from "@/views/components/navbar";
 import ToastContainer from "@/views/components/toast-container";
 
 export default {
@@ -47,6 +19,7 @@ export default {
 
     components: {
         "loading-layer": LoadingLayer,
+        "navbar": Navbar,
         "toast-container": ToastContainer,
     },
 };
