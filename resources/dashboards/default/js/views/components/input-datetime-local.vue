1729aac71492f1af575a52ab3537a12d5508cf8f
russsiq <rusiq@list.ru>
2021-02-27 13:19:22 +1000

    Create input-datetime-local.vue

---
+++ resources/dashboards/default/js/views/components/input-datetime-local.vue
@@ -0,0 +1,36 @@
+<template>
+    <input type="datetime-local" :value="localValue" @input="onInputHandler" />
+</template>
+
+<script>
+export default {
+    name: "input-datetime-local",
+
+    props: {
+        value: {
+            type: [String, Date],
+            default: null,
+            required: false,
+        },
+    },
+
+    data() {
+        return {};
+    },
+
+    computed: {
+        localValue() {
+            return (
+                this.$props.value &&
+                    this.$props.value.replace(" ", "T").slice(0, 19)
+            );
+        },
+    },
+
+    methods: {
+        onInputHandler(event) {
+            this.$emit("input", event.target.value);
+        },
+    },
+};
+</script>
