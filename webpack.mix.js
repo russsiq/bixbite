69671c99f6a56d21647616466fd39e3281c1a191
russsiq <rusiq@list.ru>
2021-03-09 18:12:34 +1000

    Update webpack.mix.js

--- webpack.mix.js
+++ webpack.mix.js
@@ -14,24 +14,29 @@ mix.options({
 
 if (isDashboard) {
     const DASHBOARD = process.env.APP_DASHBOARD;
-    const DASHBOARD_PATH = `./resources/dashboards/${DASHBOARD}/`;
+    const DASHBOARD_PATH = `${__dirname}/resources/dashboards/${DASHBOARD}`;
 
     mix.alias({
-        '@': `${__dirname}/resources/dashboards/${DASHBOARD}/js`,
-    })
-        .sass(`${DASHBOARD_PATH}sass/dashboard.scss`, 'public/css')
-        .js(`${DASHBOARD_PATH}js/dashboard.js`, 'public/js')
+            '@': `${DASHBOARD_PATH}/js`,
+        })
+        .sass(`${DASHBOARD_PATH}/sass/dashboard.scss`, 'public/css')
+        .js(`${DASHBOARD_PATH}/js/dashboard.js`, 'public/js')
         .vue();
 } else {
     const THEME = process.env.APP_THEME;
-    const THEME_PATH = `./resources/themes/${THEME}/`;
+    const THEME_PATH = `${__dirname}/resources/themes/${THEME}`;
 
-    mix.sass(`${THEME_PATH}sass/app.scss`, 'public/css')
-        .js(`${THEME_PATH}js/app.js`, 'public/js');
+    mix.sass(`${THEME_PATH}/sass/app.scss`, 'public/css')
+        .js(`${THEME_PATH}/js/app.js`, 'public/js');
 }
 
 // mix.postCss('resources/css/app.css', 'public/css', [
 //     //
 // ])
 
-mix.inProduction() && mix.version();
+mix.version([
+    'public/js/app.js',
+    'public/css/app.css',
+    'public/js/dashboard.js',
+    'public/css/dashboard.css',
+]);
