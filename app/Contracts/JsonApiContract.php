b3187a9dc9a798fa2f8fbca9fce6e61b40472787
russsiq <rusiq@list.ru>
2021-03-18 15:39:55 +1000

    Update JsonApiContract.php

--- app/Contracts/JsonApiContract.php
+++ app/Contracts/JsonApiContract.php
@@ -10,12 +10,12 @@ interface JsonApiContract
     public const HEADER_RESOURCE = 'X-JSON-API-RESOURCE';
     public const ROUTE_API_VERSION = 'v1';
     public const RESORCE_TO_MODEL_MAP = [
-        'articles' => \App\Models\Article::class,
-        'atachments' => \App\Models\Atachment::class,
-        'comments' => \App\Models\Comment::class,
-        'categories' => \App\Models\Category::class,
-        'tags' => \App\Models\Tag::class,
-        'users' => \App\Models\User::class,
+        \App\Models\Article::TABLE => \App\Models\Article::class,
+        \App\Models\Atachment::TABLE => \App\Models\Atachment::class,
+        \App\Models\Comment::TABLE => \App\Models\Comment::class,
+        \App\Models\Category::TABLE => \App\Models\Category::class,
+        \App\Models\Tag::TABLE => \App\Models\Tag::class,
+        \App\Models\User::TABLE => \App\Models\User::class,
     ];
     public const SPEC_VERSION = '1.1';
 
