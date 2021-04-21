a3c2332b6641501699f963b6d6a413c772d98ce2
russsiq <rusiq@list.ru>
2021-04-22 01:19:29 +1000

    Обновит языковые файлы

--- resources/lang/ru/pagination.php
+++ resources/lang/ru/pagination.php
@@ -1,20 +1,17 @@
 <?php
 
 return [
-
     /*
     |--------------------------------------------------------------------------
-    | Pagination Language Lines
+    | Языковые ресурсы постраничного вывода
     |--------------------------------------------------------------------------
     |
-    | The following language lines are used by the paginator library to build
-    | the simple pagination links. You are free to change them to anything
-    | you want to customize your views to better match your application.
+    | Последующие языковые строки используются библиотекой постраничного вывода
+    | для создания простых ссылок на страницы. Вы можете поменять их на любые
+    | другие, которые лучше подходят для вашего приложения.
     |
     */
 
-    'page' => 'Страница',
     'previous' => '&laquo; Назад',
-    'next' => 'Далее &raquo;',
-
+    'next'     => 'Вперёд &raquo;',
 ];
