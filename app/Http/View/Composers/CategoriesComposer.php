01c8d26324c3e9563dbf51973842e20d177963a7
russsiq <rusiq@list.ru>
2021-04-22 00:38:49 +1000

    Добавит компоновщика шаблона `CategoriesComposer`

---
+++ app/Http/View/Composers/CategoriesComposer.php
@@ -0,0 +1,63 @@
+<?php
+
+namespace App\Http\View\Composers;
+
+use App\Models\Category;
+use App\Models\Collections\CategoryCollection;
+use Illuminate\Contracts\View\View as ViewContract;
+
+class CategoriesComposer
+{
+    /** @var Category */
+    protected $categoryModel;
+
+    /** @var CategoryCollection */
+    protected $categoryCollection;
+
+    /**
+     * Create a new categories composer.
+     *
+     * @param Category $categoryModel
+     */
+    public function __construct(Category $categoryModel)
+    {
+        $this->categoryModel = $categoryModel;
+    }
+
+    /**
+     * Bind data to the view.
+     *
+     * @param  ViewContract  $view
+     * @return void
+     */
+    public function compose(ViewContract $view): void
+    {
+        $view->with(
+            'categories', $this->categoryCollection()
+        );
+    }
+
+    /**
+     * Get Category Collection.
+     *
+     * @return CategoryCollection
+     */
+    protected function categoryCollection(): CategoryCollection
+    {
+        return $this->categoryCollection
+            ?? $this->resolveCategoryCollection();
+    }
+
+    /**
+     * Resolve Category Collection.
+     *
+     * @return CategoryCollection
+     */
+    protected function resolveCategoryCollection(): CategoryCollection
+    {
+        return $this->categoryCollection = $this->categoryModel->query()
+            ->orderBy('position')
+            ->get()
+            ->nested();
+    }
+}
