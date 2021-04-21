a271fbe6c6078ef18130074d02f80d6c19daa28a
russsiq <rusiq@list.ru>
2021-04-21 22:19:50 +1000

    Сбросит миграции поставляемые с Laravel

--- database/migrations/2014_10_12_100000_create_password_resets_table.php
+++ database/migrations/2014_10_12_100000_create_password_resets_table.php
@@ -1,26 +1,28 @@
 <?php
 
-use Illuminate\Support\Facades\Schema;
-use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
 
 class CreatePasswordResetsTable extends Migration
 {
     /**
-     * Запустить миграции.
+     * Run the migrations.
+     *
      * @return void
      */
     public function up()
     {
         Schema::create('password_resets', function (Blueprint $table) {
-            $table->string('email', 100)->index();
+            $table->string('email')->index();
             $table->string('token');
             $table->timestamp('created_at')->nullable();
         });
     }
 
     /**
-     * Обратить миграции.
+     * Reverse the migrations.
+     *
      * @return void
      */
     public function down()
--- database/migrations/2018_05_01_000020_add_bixbite_columns_to_users_table.php
+++ database/migrations/2018_05_01_000020_add_bixbite_columns_to_users_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateUsersTable extends Migration
+class AddBixbiteColumnsToUsersTable extends Migration
 {
     /**
      * Запустить миграции.
@@ -13,24 +13,13 @@ class CreateUsersTable extends Migration
     public function up()
     {
         Schema::create('users', function (Blueprint $table) {
-            $table->id();
-            $table->string('api_token', 80)->nullable()->unique();
-            $table->rememberToken()->unique();
-            $table->string('name', 100)->unique();
-            $table->string('email', 100)->unique();
-            $table->string('password');
             $table->string('role', 20)->default('user');
             $table->string('avatar', 100)->nullable();
             $table->text('info')->length(500)->nullable();
             $table->string('where_from')->nullable();
             $table->ipAddress('last_ip')->nullable();
-            $table->timestamps(); // registered_at and logined_at
             $table->timestamp('logined_at')->nullable();
-            $table->timestamp('email_verified_at')->nullable();
             $table->timestamp('banned_until')->nullable();
-
-            $table->index('name');
-            $table->index('password');
         });
     }
 
