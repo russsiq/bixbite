431ab2d2275944599c7d5371995c6c696922beab
russsiq <rusiq@list.ru>
2021-04-22 12:36:09 +1000

    Добавит секцию тега `title` в основной макет

--- resources/themes/default/views/layouts/app.blade.php
+++ resources/themes/default/views/layouts/app.blade.php
@@ -2,7 +2,7 @@
 <html lang="{{ pageinfo('locale') }}">
 
 <head>
-    <title>{{ pageinfo()->makeTitles() }}</title>
+    <title>@yield('title', pageinfo()->makeTitles())</title>
     <meta charset="utf-8" />
     <meta http-equiv="X-UA-Compatible" content="IE=edge" />
     <meta name="viewport" content="width=device-width, initial-scale=1" />
