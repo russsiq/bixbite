0fba2c5411f1877c23bfcf6d5732cf62fdaf3233
russsiq <rusiq@list.ru>
2021-04-22 13:11:15 +1000

    Update UserProfileController.php

--- app/Http/Controllers/UserProfileController.php
+++ app/Http/Controllers/UserProfileController.php
@@ -4,19 +4,26 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Illuminate\Routing\Controller;
+use Illuminate\Contracts\Container\Container as ContainerContract;
 
 class UserProfileController extends Controller
 {
     /**
      * Show the user profile screen.
      *
+     * @param  \Illuminate\Http\ContainerContract  $container
      * @param  \Illuminate\Http\Request  $request
      * @return \Illuminate\View\View
      */
-    public function show(Request $request)
+    public function show(ContainerContract $container, Request $request)
     {
-        return view('profile.show', [
-            'user' => $request->user(),
-        ]);
+        // return view('profile.show', [
+        //     'user' => $request->user(),
+        // ]);
+
+        return $container->makeWith(UsersController::class)
+            ->profile(
+                $request->user()->id
+            );
     }
 }
