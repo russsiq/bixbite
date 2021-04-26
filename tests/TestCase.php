84482a07266ad80a1648aaccd58642d1b32753b6
russsiq <rusiq@list.ru>
2021-04-27 02:13:28 +1000

    Update TestCase.php

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -4,10 +4,30 @@ namespace Tests;
 
 use App\Models\User;
 use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
+use Laravel\Sanctum\Sanctum;
 
 abstract class TestCase extends BaseTestCase
 {
     use CreatesApplication;
+
+    protected function loginSPA(array $attributes = [], array $abilities = []): User
+    {
+        $user = Sanctum::actingAs(
+            $this->createUser($attributes),
+            $abilities
+        );
+
+        return $user;
+    }
+
+    protected function loginSuperAdminSPA(array $attributes = [], array $abilities = []): User
+    {
+        [$first_email] = explode(',', env('APP_SUPER_ADMINS'), 2);
+
+        $attributes['email'] = $first_email;
+
+        return $this->loginSPA($attributes, $abilities);
+    }
 
     protected function createUser(array $attributes = []): User
     {
