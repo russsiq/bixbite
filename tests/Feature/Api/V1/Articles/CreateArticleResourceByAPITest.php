d386ce4175296848f008b918b524144fcd9046fe
russsiq <rusiq@list.ru>
2021-04-27 02:15:56 +1000

    Create CreateArticleResourceByAPITest.php

---
+++ tests/Feature/Api/V1/Articles/CreateArticleResourceByAPITest.php
@@ -0,0 +1,66 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Tests\Feature\Api\V1\Articles;
+
+use App\Models\Article;
+use App\Models\User;
+use App\Policies\ArticlePolicy;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Http\JsonResponse;
+use Tests\Concerns\InteractsWithPolicy;
+use Tests\Concerns\JsonApiTrait;
+use Tests\Feature\Api\V1\Articles\Fixtures\ArticleFixtures;
+use Tests\TestCase;
+
+/**
+ * Тестирование создания ресурса `Article`.
+ *
+ * @cmd vendor/bin/phpunit Tests\Feature\Api\V1\Articles\CreateArticleResourceByAPITest.php
+ */
+class CreateArticleResourceByAPITest extends TestCase
+{
+    use JsonApiTrait;
+    use InteractsWithPolicy;
+    use RefreshDatabase;
+
+    public const JSON_API_RESOURCE = 'articles';
+
+    public function test_guest_cannot_create_article()
+    {
+        $response = $this->assertGuest()
+            ->postJsonApi('store', [], [
+
+            ])
+            ->assertStatus(JsonResponse::HTTP_UNAUTHORIZED);
+    }
+
+    public function test_user_without_ability_cannot_create_article()
+    {
+        $this->denyPolicyAbility(ArticlePolicy::class, ['create']);
+
+        $user = $this->loginSPA();
+
+        $response = $this->assertAuthenticated()
+            ->postJsonApi('store', [], [
+
+            ])
+            ->assertStatus(JsonResponse::HTTP_FORBIDDEN);
+    }
+
+    public function test_super_admin_can_create_article()
+    {
+        $super_admin = $this->loginSuperAdminSPA();
+
+        $response = $this->assertAuthenticated()
+            ->postJsonApi('store', [], [
+                'title' => 'New article title',
+            ])
+            ->assertStatus(JsonResponse::HTTP_CREATED)
+            ->assertJsonStructure(
+                ArticleFixtures::resource()
+            );
+    }
+}
