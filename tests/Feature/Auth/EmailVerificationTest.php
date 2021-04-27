ec12503482c8ff642fad4710bb126a5a653f0d60
russsiq <rusiq@list.ru>
2021-04-27 23:14:18 +1000

    Update EmailVerificationTest.php

--- tests/Feature/Auth/EmailVerificationTest.php
+++ tests/Feature/Auth/EmailVerificationTest.php
@@ -2,7 +2,6 @@
 
 namespace Tests\Feature\Auth;
 
-use App\Models\User;
 use App\Providers\RouteServiceProvider;
 use Illuminate\Auth\Events\Verified;
 use Illuminate\Foundation\Testing\RefreshDatabase;
