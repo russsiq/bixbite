8d93586f112765cb913ec6ab3672981186abd29e
russsiq <rusiq@list.ru>
2021-04-28 00:00:09 +1000

    Update CreateArticleResourceByAPITest.php

--- tests/Feature/Api/V1/Articles/CreateArticleResourceByAPITest.php
+++ tests/Feature/Api/V1/Articles/CreateArticleResourceByAPITest.php
@@ -15,11 +15,7 @@ use Tests\Concerns\JsonApiTrait;
 use Tests\Feature\Api\V1\Articles\Fixtures\ArticleFixtures;
 use Tests\TestCase;
 
-/**
- * Тестирование создания ресурса `Article`.
- *
- * @cmd vendor/bin/phpunit Tests\Feature\Api\V1\Articles\CreateArticleResourceByAPITest.php
- */
+/** @cmd vendor/bin/phpunit Tests\Feature\Api\V1\Articles\CreateArticleResourceByAPITest.php */
 class CreateArticleResourceByAPITest extends TestCase
 {
     use JsonApiTrait;
