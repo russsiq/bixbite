25e7d7e5aec01c3ffe2f7fb510a7965fc61a467f
russsiq <rusiq@list.ru>
2021-04-27 23:19:23 +1000

    Update PasswordResetTest.php

--- tests/Feature/Auth/PasswordResetTest.php
+++ tests/Feature/Auth/PasswordResetTest.php
@@ -2,7 +2,6 @@
 
 namespace Tests\Feature\Auth;
 
-use App\Models\User;
 use Illuminate\Auth\Notifications\ResetPassword;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Support\Facades\Notification;
