92c0bc87a573cf93e4212be712bc6e03b85dde83
russsiq <rusiq@list.ru>
2021-05-24 09:40:51 +1000

    Update BelongsToUser.php

--- app/Models/Relations/BelongsToUser.php
+++ app/Models/Relations/BelongsToUser.php
@@ -5,8 +5,16 @@ namespace App\Models\Relations;
 use App\Models\User;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
+/**
+ * @property-read \App\Models\User $user
+ */
 trait BelongsToUser
 {
+    /**
+     * Get the user that owns the current model instance.
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
+     */
     public function user(): BelongsTo
     {
         return $this->belongsTo(User::class, 'user_id', 'id', 'user');
