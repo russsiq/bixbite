2e7df3d3e412c1bc236449d68539190903ebc46c
russsiq <rusiq@list.ru>
2021-07-02 22:53:48 +1000

    wip

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -3,7 +3,11 @@
 namespace App\Http\Controllers;
 
 use App\Contracts\BixBiteContract;
+use Illuminate\Config\Repository as ConfigRepository;
 use Illuminate\Contracts\Foundation\Application;
+use Illuminate\Contracts\View\Factory as ViewFactoryContract;
+use Illuminate\Contracts\View\View;
+use Illuminate\Http\Request;
 use Illuminate\Routing\Controller;
 
 class DashboardController extends Controller
@@ -16,20 +20,50 @@ class DashboardController extends Controller
      */
     public function __invoke(
         Application $app,
-        BixBiteContract $bixbite
-    ) {
-        $app->view->addLocation(
+        BixBiteContract $bixbite,
+        ConfigRepository $config,
+        Request $request,
+        ViewFactoryContract $view,
+    ): View {
+        $view->addLocation(
             $bixbite->dashboardViewsPath()
         );
 
-        return $app->view->make('dashboard', [
-            // 'scriptVariables' => [
-            //     'api_url' => $app->url->to(JsonApi::API_URL),
-            //     'app_name' => $app->config->get('app.name'),
-            //     'app_url' => $app->url->route('home'),
+        return $view->make('dashboard', [
+            'scriptVariables' => [
+                'page_language' => str_replace('_', '-', $app->getLocale()),
+                'app_name' => $config->get('app.name'),
+                'app_locale' => $app->getLocale(),
+                // 'locale' => $bixbite->locale(),
+                'app_url' => setting('system.app_url', url()->to('/')),
+                'api_url' => url()->to('/api/v1'),
             //     'dashboard_base_url' => 'dashboard',
-            //     'locale' => $bixbite->locale(),
-            // ],
+            ],
         ]);
+    }
+
+    /**
+     * Get the variables for vue.js or another javascript.
+     *
+     * @return string
+     */
+    public function scriptVariables()
+    {
+        $data = json_encode([
+            'locale' => $this->get('locale'),
+            'csrf_token' => $this->get('csrf_token'),
+            'app_name' => $this->get('app_name'),
+            'app_dashboard' => $this->get('app_dashboard'),
+            'app_theme' => $this->get('app_theme'),
+            'app_url' => $this->get('app_url'),
+            'api_url' => $this->get('api_url'),
+            'dashboard' => $this->get('dashboard'),
+        ]);
+
+        if (JSON_ERROR_NONE !== json_last_error()) {
+            throw new RuntimeException(json_last_error_msg());
+        }
+
+        return $data;
     }
 }
